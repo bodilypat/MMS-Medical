28216d4e562e7540f0e2fabec0ba4ece1a16d275
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-20 15:46:53 -0700

    Add files via upload

---
+++ doctor/logout.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    include('include/config.php');
+    $_SESSION['dlogin']=="";
+    date_default_timezone_set('America/Los_angeles');
+    $ldate=date( 'd-m-Y h:i:s A', time () );
+    $docid=$_SESSION['id'];
+    mysqli_query($deal,"UPDATE doctorslog  SET logout = '$ldate' WHERE uid = '$docid' ORDER BY id DESC LIMIT 1");
+    session_unset();
+    //session_destroy();
+    $_SESSION['errmsg']="You have successfully logout";
+?>
+<script language="javascript">
+do  cument.location="../../index.php";
+</script>
