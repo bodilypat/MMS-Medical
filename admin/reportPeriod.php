84f4009e5436093ba09b1c506de320a41716f254
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-26 06:09:42 -0700

    Add files via upload

--- admin/reportPeriod.php
+++ admin/reportPeriod.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	error_reporting(0);
-	include('include/config.php');
+	include('../define/config.php');
 	if(strlen($_SESSION['id']==0)) {
 	header('location:logout.php');
 	} else{
@@ -10,19 +10,19 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Admin | Info Patients</title>
+		<title>Admin | Information Patients</title>
 		<!-- CSS  -->
-		<link rel="stylesheet" href="assets/css/styles.css">
-		<link rel="stylesheet" href="assets/css/plugins.css">
-		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+		<link rel="stylesheet" href="../assign/css/styles.css">
+		<link rel="stylesheet" href="../assign/css/plugins.css">
+		<link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
 	</head>
 <body>
-	<div id="app">		
-		<?php include('include/sidebar.php');?>
-		<div class="app-content">
-			<?php include('include/header.php');?>
+	<div id="application">		
+		<?php include('../define/sidebar.php');?>
+		<div class="application-content">
+			<?php include('../define/header.php');?>
 			<div class="main-content" >
-				<div class="wrap-content container" id="container">
+				<div id="container" class="wrap-content container" >
 						<!-- section: PAGE TITLE -->
 					<section id="page-title">
 						<div class="row">
@@ -87,11 +87,11 @@
 		</div>
 	</div>
 	<!-- FOOTER -->
-	<?php include('include/footer.php');?>
-	<?php include('include/setting.php');?>
+	<?php include('../define/footer.php');?>
+	<?php include('../define/setting.php');?>
 	<!-- JAVASCRIP -->
-	<script src="assets/js/main.js"></script>
-	<script src="assets/js/form-elements.js"></script>
+	<script src="../assign/js/main.js"></script>
+	<script src="../assign/js/form-elements.js"></script>
 	<script>
 		jQuery(document).ready(function() {
 			Main.init();
