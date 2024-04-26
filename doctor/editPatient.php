6ced6be5e4adffd3560ff93720b1aecffb8d32e8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-26 07:08:42 -0700

    Add files via upload

--- doctor/editPatient.php
+++ doctor/editPatient.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	error_reporting(0);
-	include('include/config.php');
+	include('../define/config.php');
 	if(strlen($_SESSION['id']==0))
 	{
 		header('location:logout.php');
@@ -36,23 +36,23 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Doctor | Add Patient</title>
+		<title>Doctor | Edit Patient</title>
 		<!-- CSS -->
-		<link rel="stylesheet" href="assets/css/styles.css">
-		<link rel="stylesheet" href="assets/css/plugins.css">
-		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+		<link rel="stylesheet" href="../assign/css/styles.css">
+		<link rel="stylesheet" href="../assign/css/plugins.css">
+		<link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
 	</head>
 <body>
 	<div id="application">		
-		<?php include('include/sidebar.php');?>
+		<?php include('../define/sidebar.php');?>
 		<div class="application-content">
-			<?php include('include/header.php');?>						
+			<?php include('../define/header.php');?>						
 			<div class="main-content" >
-				<div class="wrap-content container" id="container">
+				<div id="container" class="wrap-content container" >
 					<!-- section: PAGE TITLE -->
 					<section id="page-title">
 						<div class="row">
-							<div class="col-sm-8"><h1 class="mainTitle">Patient | Add Patient</h1></div>
+							<div class="col-sm-8"><h1 class="mainTitle">Patient | Edit Patient</h1></div>
 							<ol class="breadcrumb">
 								<li><span>Patient</span></li>
 								<li class="active"><span>Add Patient</span></li>
@@ -65,35 +65,37 @@
 								<div class="row margin-top-30">
 									<div class="col-lg-8 col-md-12">
 										<div class="panel panel-white">
-											<div class="panel-heading"><h5 class="panel-title">Add Patient</h5></div>
+											<div class="panel-heading">
+												<h5 class="panel-title">Add Patient</h5>
+											</div>
 											<div class="panel-body">
 												<form name="edit-patient" role="form"  method="post">
 													<?php
 														$eid=$_GET['editid'];
 														$qPat=mysqli_query($deal,"SELECT * FROM patient WHERE ID='$eid'");
 														$count=1;
-														while ($result=mysqli_fetch_array($qPat)) 
+														while ($resultset=mysqli_fetch_array($qPat)) 
 														{
 													?>
 													<div class="form-group">
 														<label for="Doctorname">Patient Name</label>
-														<input name="patname" type="text"  class="form-control"  
-														       value="<?php  echo $result['PatientName'];?>" required="true">
+														<input name="patientname" type="text"  class="form-control"  
+														       value="<?php  echo $resultset['PatientName'];?>" required="true">
 													</div>
 													<div class="form-group">
 														<label for="PatientContact">Patient Contact no</label>
-														<input name="patcontact" type="text"  class="form-control"  
+														<input name="patientcontact" type="text"  class="form-control"  
 															   value="<?php  echo $resultset['PatientContno'];?>" 
 															   required="true" maxlength="10" pattern="[0-9]+">
 													</div>
 													<div class="form-group">
 														<label for="PatientEmail">Patient Email</label>
-														<input id="patientemail" name="patientemail" type="email"  class="form-control"  value="<?php  echo $row['PatientEmail'];?>" readonly='true'>
+														<input id="patientemail" name="patientemail" type="email" class="form-control"  value="<?php  echo $row['PatientEmail'];?>" readonly='true'>
 														<span id="email-availability-status"></span>
 													</div>
 													<div class="form-group">
 														<label class="control-label">Gender: </label>
-           												   <?php  if($result['Gender']=="Female"){ ?>
+           												   <?php  if($resultset['Gender']=="Female"){ ?>
 														<input name="gender" id="gender" type="radio"  value="Female" checked="true">Female
 														<input name="gender" id="gender" type="radio"  value="male">Male
             											  <?php } else { ?>
@@ -106,25 +108,25 @@
 													<div class="form-group">
 														<label for="PatientAddress">Patient Address</label>
 														<textarea name="patientaddress" class="form-control" required="true">
-															     <?php  echo $result['PatientAdd'];?>
+															     <?php  echo $resulset['PatientAdd'];?>
 														</textarea>
 													</div>
 													<div class="form-group">
 														<label for="PatientAge">Patient Age</label>
-														<input name="patage" type="text"  class="form-control"  
-														       value="<?php  echo $result['PatientAge'];?>" required="true">
+														<input name="patientage" type="text"  class="form-control"  
+														       value="<?php  echo $resultset['PatientAge'];?>" required="true">
 													</div>
 													<div class="form-group">
 														<label for="MedicalHistory">Medical History</label>
 														<textarea name="medhis" type="text"  class="form-control"  
 														          placeholder="Patient Medical History(if any)" required="true">
-																  <?php  echo $result['PatientMedhis'];?>
+																  <?php  echo $resultset['PatientMedhis'];?>
 														</textarea>
 													</div>	
 													<div class="form-group">
 														<label for="fess">Creation Date</label>
 														<input type="text" class="form-control"  
-														       value="<?php  echo $row['CreationDate'];?>" readonly='true'>
+														       value="<?php  echo $resultset['CreationDate'];?>" readonly='true'>
 													</div>
 													<?php } ?>
 													<button name="submit" id="submit" type="submit"  class="btn btn-o btn-primary">Update</button>
@@ -143,11 +145,11 @@
 		</div>				
 	</div>
 	<!-- FOOTER -->
-	<?php include('include/footer.php');?>	
-	<?php include('include/setting.php');?>	
+	<?php include('../define/footer.php');?>	
+	<?php include('../define/setting.php');?>	
 	<!-- JAVASCRIPT -->
-	<script src="assets/js/main.js"></script>
-	<script src="assets/js/form-elements.js"></script>
+	<script src="../assign/js/main.js"></script>
+	<script src="../assign/js/form-elements.js"></script>
 	<script>
 		jQuery(document).ready(function() {
 			Main.init();
