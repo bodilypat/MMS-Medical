a71b1d2ad86a3ed934084c7af318982cdb175f57
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-26 08:48:11 -0700

    Add files via upload

--- patient/logout.php
+++ patient/logout.php
@@ -1,13 +1,12 @@
 <?php
     session_start();
-    include('include/config.php');
+    include('../define/config.php');
     $_SESSION['login']=="";
     date_default_timezone_set('America/Los_Angeles');
     $ldate=date( 'd-m-Y h:i:s A', time () );
             mysqli_query($deal,"UPDATE userlog  SET logout = '$ldate' 
                                WHERE uid = '".$_SESSION['id']."' ORDER BY id DESC LIMIT 1");
-            session_unset();
-    //session_destroy();    
+            session_unset(); 
     $_SESSION['errmsg']="You have successfully logout";
 ?>
 <script language="javascript">
