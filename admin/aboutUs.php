a7402d2dfe88ffcaad4a3c210d2736a6fdd99022
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 20:04:05 -0700

    Add files via upload

--- admin/aboutUs.php
+++ admin/aboutUs.php
@@ -1,15 +1,15 @@
 <?php
 	session_start();
 	error_reporting(0);
-	include('include/config.php');
+	include('../define/config.php');
 	if(strlen($_SESSION['id']==0)) {
 		header('location:logout.php');
 		} else{
-			// Update the Content
 			if(isset($_POST['submit']))
 			{   
 				$pageTitle=$_POST['pagetitle'];
 				$pageDes=$deal->real_escape_string($_POST['pageDescript']);
+				/* update tblpage */
 				$qPage=mysqli_query($deal,"UPDATE tblpage 
 				                           SET PageTitle='$pageTitle',PageDescription='$pageDes' 
 										   WHERE  PageType='aboutus'");
@@ -26,16 +26,16 @@
 <html lang="en">
 	<head>
 		<title>Admin | About Us </title>
-		<!-- CSS -->
+		<!-- custom CSS -->
 		<link rel="stylesheet" href="assets/css/styles.css">
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 </head>
 <body>
 	<div id="app">		
-		<?php include('include/sidebar.php');?>
+		<?php include('../define/sidebar.php');?>
 		<div class="app-content">
-			<?php include('include/header.php');?>
+			<?php include('../define/header.php');?>
 			<div class="main-content" >
 				<div class="wrap-content container" id="container">
 					<!-- section: PAGE TITLE -->
@@ -53,25 +53,26 @@
 							<div class="col-md-12">						
 								<form class="forms-sample" method="post">
 									<?php
+										/* query tblpage from DB */
 										$qPage=mysqli_query($deal,"SELECT * FROM  tblpage WHERE PageType='aboutus'");
 										$count=1;
-										while ($resultpage=mysqli_fetch_array($qPage)) 
+										while ($resultset=mysqli_fetch_array($qPage)) 
 										{
 									?>
 									<div class="form-group">
-										<label for="exampleInputUsername1">Page Title</label>
+										<label for="PageTitle">Page Title</label>
 										<input id="pagetitle" name="pagetitle" type="text" class="form-control" required="true" 
-										        value="<?php  echo $resultpage['PageTitle'];?>">
+										        value="<?php  echo $resultset['PageTitle'];?>">
 									</div>													
 									<div class="form-group">
-										<label for="exampleInputEmail1">Page Description</label>
-										<textarea class="form-control" name="pagedes" id="pagedes" rows="12">
-											<?php  echo $resultpage['PageDescription'];?>
+										<label for="PageDescription">Page Description</label>
+										<textarea id="pagedes" name="pagedes" class="form-control" rows="12">
+											<?php  echo $resultset['PageDescription'];?>
 										</textarea>
 									</div>										
 									<?php } ?>
 									<!--end :while loop -->
-									<button type="submit" class="btn btn-primary mr-2" name="submit">Submit</button>
+									<button name="submit" type="submit" class="btn btn-primary mr-2" >Submit</button>
 								</form>
 							</div>
 						</div>
@@ -79,13 +80,13 @@
 				</div>
 			</div>
 			<!-- FOOTER -->
-			<?php include('include/footer.php');?>			
-			<?php include('include/setting.php');?>						
+			<?php include('../define/footer.php');?>			
+			<?php include('../define/setting.php');?>						
 		</div>
-	</div>		
-	<script src="assets/js/main.js"></script>
-	<!-- JAVASCRIPT -->
-	<script src="assets/js/form-elements.js"></script>
+	</div>	
+	<!-- JAVASCRIPT -->	
+	<script src="assign/js/main.js"></script>
+	<script src="assign/js/form-elements.js"></script>
 	<script>
 		jQuery(document).ready(function() {
 			Main.init();
