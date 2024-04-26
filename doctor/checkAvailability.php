5202f2740895b719719d1659b9eac680ff7f4a15
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-26 07:32:54 -0700

    Add files via upload

--- doctor/checkAvailability.php
+++ doctor/checkAvailability.php
@@ -1,10 +1,10 @@
 <?php 
-	require_once("include/config.php");
+	require_once("../define/config.php");
 	if(!empty($_POST["email"])) {
 	$email= $_POST["email"];
-	$qPat=mysqli_query($deal,"SELECT PatientEmail FROM tblpatient WHERE PatientEmail='$email'");
-	$result=mysqli_num_rows($qPat);
-	if($result > 0)
+	$qPat=mysqli_query($deal,"SELECT PatientEmail FROM patient WHERE PatientEmail='$email'");
+	$countRow=mysqli_num_rows($qPat);
+	if($countRow > 0)
 	{
 		echo "<span style='color:red'> Email already exists .</span>";
  		echo "<script>$('#submit').prop('disabled',true);</script>";
