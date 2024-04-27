4de8a13f8d83b2f2a272bdb4630732c4651d9b21
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-27 16:14:46 -0700

    Add files via upload

--- patient/resetPassword.php
+++ patient/resetPassword.php
@@ -1,94 +1,94 @@
 <?php
-	session_start();
-	include("../define/config.php");
-	/* Code for updating Password */
-	if(isset($_POST['change']))
-	{
-		$userName=$_SESSION['name'];
-		$userEmail=$_SESSION['email'];
-		$newPass=md5($_POST['password']);
-
-		$qUser=mysqli_query($deal,"UPDATE users SET password='$newPass' 
-		                           WHERE fullName='$userName' AND email='$userEmail'");
-		if ($qUser) {
-			echo "<script>alert('Password successfully updated.');</script>";
-			echo "<script>window.location.href ='userLogin.php'</script>";
-	}
-}
-
+    session_start();
+    include('../assign/config.php');
+    /* code updating password */
+    if(isset($_POST['change']))
+    {
+        $userName = $_SESSIONN['fullName'];
+        $userEmail = $_SESSION['email'];
+        $nPass = md5($_POST['password']);
+        $qUser = mysqli_query($dbcon,"UPDATE users SET password='$nPass' WHERE fullname='$userName' AND email='$email' ");
+        if($qUser)
+        {
+            echo '<script>alert("Password successfully updated.");</script>';
+            echo '<script>window.location.href ="userLogin.php"</script>';
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Password Reset</title>
-		<!-- CSS -->
-		<link rel="stylesheet" href="../assign/css/styles.css">
-		<link rel="stylesheet" href="../assign/css/plugins.css">
-		<link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
-
-		<script type="text/javascript">
-			function valid()
-			{
-				if(document.passwordreset.password.value!= document.passwordreset.cfpassword.value)
-				{
-					alert("Password and Confirm Password Field do not match  !!");
-					document.passwordreset.cfpassword.focus();
-					return false;
-				}
-				return true;
-			}
-		</script>
-	</head>
-<body class="login">
-	<div class="row">
-		<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-			<div class="logo margin-top-30"><a href="../index.php"><h2> MMS | Patient Reset Password</h2></a></div>
-				<div class="box-login">
-					<form name="passwordreset" class="form-login"  method="post" onSubmit="return valid();">
-						<fieldset>
-							<legend>Patient Reset Password</legend>
-							<p>Please set your new password.<br />
-								<span style="color:red;">
-								      <?php echo $_SESSION['errmsg']; ?>
-									  <?php echo $_SESSION['errmsg']="";?>
-								</span>
-							</p>
-							<div class="form-group">
-								<span class="input-icon">
-									<input id="password" name="password" type="password" class="form-control"  placeholder="Password" required>
-										<i class="fa fa-lock"></i> 
-								</span>
-							</div>
-							<div class="form-group">
-								<span class="input-icon">
-									<input id="cfpassword" name="cfpassword" type="password" class="form-control"   placeholder="Password Again" required>
-										<i class="fa fa-lock"></i> 
-								</span>
-							</div>
-							<div class="form-actions">
-								<button name="change" type="submit" class="btn btn-primary pull-right" >
-									  Change <i class="fa fa-arrow-circle-right"></i>
-								</button>
-							</div>							
-							<div class="new-account">Already have an account? 
-								<a href="userLogin.php">Log-in</a>
-							</div>
-						</fieldset>
-					</form>
-					<div class="">
-						&copy; <span class="text-bold text-uppercase">Menage Management System</span>
-					</div>			
-				</div>
-			</div>
-		</div>
-	</div>
-	<script src="../assign/js/main.js"></script>
-	<script src="../assign/js/login.js"></script>
-	<script>
-		jQuery(document).ready(function() {
-				Main.init();
-				Login.init();
-		});
-	</script>
-	</body>
+    <head>
+        <title>Password Reset</title>
+        <!-- custom style -->
+        <link rel="stylesheet" href="assign/css/styles.css">
+        <link rel="styelsheet" href="assign/css/plugins.css">
+        <link rel="stylesheet" href="assign/css/theme-1.css" id="skin-color">
+        <script typ="text/javascript">
+            function valid()
+            {
+                 if(document.passwordreset.password.value != document.passwordreset.cfpassword.value)
+                 {
+                    alert("Password and Confirm Password Field do not match !!");
+                    document.passwordreset.cfpassword.focus();
+                    return false;
+                 }
+                 return true;
+            }
+        </script>
+    </head>
+    <body class="login">
+        <div class="row">
+            <div class="main-login">
+               <div class="logo margin-top-30">
+                     <a href="../index.php"><h2>MMS | Patient Reset Password</h2></a>
+               </div>
+               <div class="box-login">
+                     <form name="passwordreset" method="post" class="form-login" onSubmit="return valid();">
+                            <fieldset>
+                                <legend>Patient Reset Password</legend>
+                                <p>Please set you Password.<br >
+                                    <span style="color:red;">
+                                        <?php echo $_SESSION['errmsg']; ?>
+                                        <?php echo $_SESSION['errmsg']=""; ?>
+                                    </span>
+                                </p>
+                                <div class="form-group">
+                                      <span class="input-icon">
+                                            <input name="password" id="password" type="password" class="form-control" placeholder="Password" required>
+                                            <i class="fa fa-lock"></i>
+                                      </span>
+                                </div>
+                                <div class="form-group">
+                                      <span class="input-icon">
+                                            <input name="cfpassword" id="cfpassword" type="password" class="form-control" Placeholder="Password Again" required>
+                                            <i class="fa fa-lock"></i>
+                                      </span>
+                                </div>
+                                <div class="form-actions">
+                                      <button name="change" type="submit" class="btn btn-primary pull-right">Change
+                                              <i class="fa fa-arrow-circle-right"></i>
+                                      </button>
+                                </div>
+                                <div class="new-accounrt">
+                                      Already have an account
+                                      <a href="userLogin.php">Log-in</a>
+                                </div>
+                            </fieldset>
+                    </form>
+                    <div class="">
+                           <span class="text-bold text-uppercase">Medical Management System</spam>
+                    </div>
+               </div>
+            </div>
+        </div>
+        <!-- custom javascript -->
+        <script src="assign/js/main.js"></script>
+        <script src="assign/js/login.js"></script>
+        <script>
+            jQuery(document).ready(function(){
+                Main.init();
+                login.init();
+            });
+        </script>
+    </body>
 </html>
