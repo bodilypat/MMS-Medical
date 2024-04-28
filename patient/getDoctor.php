7f88178fa340ccd423179141f229939da5662966
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-28 15:45:07 -0700

    Add files via upload

--- patient/getDoctor.php
+++ patient/getDoctor.php
@@ -1,35 +1,24 @@
 <?php
-include('../define/config.php');
-if(!empty($_POST["specilizationid"])) 
-{
-	$qDoc=mysqli_query($deal,"SELECT doctorName,id 
-							 FROM  doctors 
-							 WHERE specilization='".$_POST['specilizationid']."'");
-	?>								
-	<option selected="selected">Select Doctor </option>
-	<?php
-		 while($result=mysqli_fetch_array($qDoc))
-		{
-		?>
-			<option value="<?php echo htmlentities($result['id']); ?>">
-			               <?php echo htmlentities($result['doctorName']); ?>
-			</option>
-		<?php
-		 }
-}
-
-		if(!empty($_POST["doctor"])) 
-		{
-
-			 $qdf=mysqli_query($deal,"SELECT docFees FROM doctors WHERE id='".$_POST['doctor']."'");
-			 while($record=mysqli_fetch_array($qdf))
-			{
-			?>
-				<option value="<?php echo htmlentities($record['docFees']); ?>">
-				               <?php echo htmlentities($record['docFees']); ?>
-			    </option>
-				<?php
-			}
-		}
+    include('../define/config.php');
+    if(!empty($_POST['specialized']))
+    {
+        $qDoc=mysqli_query($dbcon,"SELECT doctorName, id From doctors WHERE special='".$_POST['specialized']."'");?>
+        <option selected="selected">Select Doctor</option>
+        <?php 
+            while($result=mysqli_fetch_array($qDoc))
+            {
+        ?>
+        <option value="<?php echo htmlentities($result['id']);?>"><?php echo htmlentities($result['doctorName']);?></option>
+        <?php  }
+    }
+    if(!empty($_POST['doctor']))
+    {
+        $qDoc=mysqli_query($dbcon,"SELECT docFees FROM doctors WHERE id='".$_POST['doctor']."'");
+        while($result=mysqli_fetch_array($qDoc))
+        {
+    ?>
+        <option value="<?php echo htmlentities($result['docFees']); ?>"><?php echo htmlentities($result['docFees']);?></option>
+        <?php
+        }
+    }
 ?>
-
