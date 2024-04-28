a335b219bd905b6d4dd4693875f0f6f314839c9f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-28 01:16:17 -0700

    Add files via upload

--- patient/checkAvailability.php
+++ patient/checkAvailability.php
@@ -1,18 +1,17 @@
-<?php 
-require_once("../define/config.php");
-
-if(!empty($_POST["email"])) 
-{
-	$userEmail= $_POST["email"];
-	$qUser =mysqli_query($deal,"SELECT email FROM users WHERE email='$userEmail'");
-	$result=mysqli_num_rows($qUser);
-	if($result>0)
-	{
-		echo "<span style='color:red'> Email already exists .</span>";
-		echo "<script>$('#submit').prop('disabled',true);</script>";
-		} else{
-			echo "<span style='color:green'> Email available for Registration .</span>";
-		echo "<script>$('#submit').prop('disabled',false);</script>";
-	}
-}
+<?php
+    require_once('../define/config.php');
+    if(!empty($_POST['EMAIL'])){
+        $email=$_POST['email'];
+        $qUser = mysqli_query($dbcon,"SELECT email from users WHERE email='$email' ");
+        $cRows = mysqli_num_rows(qUser);
+        if($cRows > 0)
+        {
+            echo "<script style='color:red'>Email already exists.</script>";
+            echo "<script>$('#submit').prop('disabled', true);</script>";
+        }else 
+        {
+            echo "<span style='color:green'>Email available for Register.</span>";
+            echo "<script>$(#submit).prop('disabled',false);</script>";
+        }
+    }
 ?>
