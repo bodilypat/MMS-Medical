aa4c3de861a958514b9e678141176ca9a4889438
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-28 16:31:09 -0700

    Add files via upload

--- patient/manageMedhistory.php
+++ patient/manageMedhistory.php
@@ -1,95 +1,94 @@
 <?php
-	session_start();
-	error_reporting(0);
-	include('../define/config.php');
-	include('../define/checklogin.php');
-	check_login();
+    session_start();
+    error_reporting(0);
+    include('../define/config.php');
+    include('../define/checkLogin');
+    check_login();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Reg Users | View Medical History</title>
-		<!-- CSS -->
-		<link rel="stylesheet" href="../assign/css/styles.css">
-		<link rel="stylesheet" href="../assign/css/plugins.css">
-		<link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
-	</head>
-<body>
-	<div id="application">		
-		<?php include('define/sidebar.php');?>
-		<div class="application-content">
-			<?php include('../define/header.php');?>
-			<div class="main-content" >
-				<div class="wrap-content container" id="container">
-					<!-- select: PATE TITLE -->
-					<section id="page-title">
-						<div class="row">
-							<div class="col-sm-8"><h1 class="mainTitle">Users | Medical History</h1></div>
-							<ol class="breadcrumb">
-								<li><span>Users</span></li>
-								<li class="active"><span>View Medical History</span></li>
-							</ol>
-						</div>
-					</section>
-					<!-- selection : CONTENT -->
-					<div class="container-fluid container-fullw bg-white">
-						<div class="row">
-							<div class="col-md-12">
-								<h5 class="over-title margin-bottom-15">View <span class="text-bold">Medical History</span></h5>										
-								<table id="patient-table" class="table table-hover" >
-									<thead>
-										<tr>
-											<th class="center">#</th>
-											<th>Patient Name</th>
-											<th>Patient Contact Number</th>
-											<th>Patient Gender </th>
-											<th>Creation Date </th>
-											<th>Updation Date </th>
-											<th>Action</th>
-										</tr>
-									</thead>
-									<tbody>											
-										<?php
-											$userID=$_SESSION['id'];
-											$qUser=mysqli_query($deal,"SELECT tblpatient.* 
-																FROM patient JOIN users ON users.email=tblpatient.PatientEmail 
-																WHERE users.id='$userID'");
-											$count=1;												
-											while($resultset=mysqli_fetch_array($qUser))
-											{
-										?>
-											<tr>
-												<td class="center"><?php echo $count;?>.</td>
-												<td class="hidden-xs"><?php echo $resultset['PatientName'];?></td>
-												<td><?php echo $resultset['PatientContno'];?></td>
-												<td><?php echo $resultset['PatientGender'];?></td>
-												<td><?php echo $resultset['CreationDate'];?></td>
-												<td><?php echo $resultset['UpdationDate'];?></td>
-												<td><a href="infoMedhistory.php?infoid=<?php echo $resultset['ID'];?>">
-													<i class="fa fa-eye"></i></a>
-												</td>
-											</tr>
-										<?php $count=$count+1; }?>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!--  FOOTER -->
-	<?php include('../define/footer.php');?>	
-	<?php include('../define/setting.php');?>
-	<!-- JAVASCRIPT -->
-	<script src="../assign/js/main.js"></script>
-	<script src="../assign/js/form-elements.js"></script>
-	<script>
-		jQuery(document).ready(function() {
-			Main.init();
-			FormElements.init();
-		});
-	</script>
-</body>
+    <head>
+        <title>Registration User |  Medicail History</title>
+        <!-- custom style -->
+        <link rel="stylesheet" href="../assign/css/styles.css">
+        <link rel="stylesheet" href="../assign/css/plugins.css">
+        <link rel="stylesheet" href="../assign/css/theme/theme-1-1.css" id="skin_color" />
+    </head>
+    <body>
+        <div id="Application">
+            <?php include('../define/sidebar.php');?>
+            <div class="application-content">
+                <?php include('../define/header.php');?>
+                <div class="main-content">
+                     <div id="container" class="wrap-content container" >
+                        <!-- section: page title -->
+                        <section id="page-title">
+                            <div class="row">
+                                <div class="col-sm-8"><h1 class="mainTitle">Users | Medical History</h1></div>
+                                <ol>
+                                     <li><span>Users</span></li>
+                                     <li class="active">Data Medical History<span></span></li>
+                                </ol>
+                            </div>
+                        </section>
+                        <!-- section: content -->
+                        <div class="container-fluid container-fullw bg-white">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h5 class="over-title margin-bottom-15">Data<span class="text-bold">Medical History</span></h5>
+                                    <table id="patient-table" class="table table-hover" >
+                                        <thead>
+                                            <tr>
+                                                <th class="center">#</th>
+                                                <th>Patient Name</th>
+                                                <th>Patient Contact Number</th>
+                                                <th>Patient Gender</th>
+                                                <th>Creation Date</th>
+                                                <th>Updation Date</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php   
+                                                $userid=$_POST['id'];
+                                                $qPat=mysqli_query($dbcon,"SELECT patients.* 
+                                                                           FROM patient JOIN users ON users.email=patientEmail 
+                                                                           WHERE users.id='$userid' ");
+                                                $count=1;
+                                                while($result=mysql_fetch_array($qPat))
+                                                {
+                                            ?>
+                                            <tr>
+                                                <td class="center"><?php echo $count;?>.</td>
+                                                <td class="hidden-xs"><?php echo $result['patientContno'];?></td>
+                                                <td><?php echo $result['patientGender'];?></td>
+                                                <td><?php echo $result['createDate'];?></td>
+                                                <td><?php echo $result['updateDate'];?></td>
+                                                <td>
+                                                    <a href="dataMedhistory.php?did=<?php echo $result['id'];?>"><i class="fa fa-eye"></i></a>
+                                                </td>
+                                            </tr>
+                                                <?php  } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+            <!-- FOOTER -->
+            <script src="../assign/js/main.js"></script>
+            <script src="../assign/js/form-elements.js"></script>
+            <!-- javascript -->
+            <script src="../assign/js/main.js"></script>
+            <script src="../assign/js/form-elements.js"></script>
+            <script>
+                jQuery(document).ready(function(){
+                    Main.init();
+                    formElements.init();
+                })
+            </script>
+        </div>
+    </body>
 </html>
