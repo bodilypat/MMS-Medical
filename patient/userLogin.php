2edfe104ccdfacfa67cfe26944269d482c615322
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-05-16 14:46:03 -0700

    Add files via upload

--- patient/userLogin.php
+++ patient/userLogin.php
@@ -1,101 +1,85 @@
 <?php
     session_start();
     error_reporting(0);
-    include("../define/config.php");
+    include('define/config.php');
     if(isset($_POST['submit']))
     {
         $userName = $_POST['username'];
         $userPass = $_POST['password'];
-        $qUser = mysqli_query($deal,"SELECT * FROM users WHERE email='$userName' AND password='$userPass' ");
-        $result = mysql_fetch_array($qUser);
-        if($result > 0)
-        {
+        $qUser=mysqli_query($db_handle,"SELECT * FROM users WHERE email = '$userName' AND password='$userPass' ");
+        $result = mysqli_query($qUser);
+        if($result > 0){
             $_SESSION['login'] = $_POST['username'];
             $_SESSION['id'] = $result['id'];
-            $userid = $result['id'];
+            $userID = $result['id'];
             $host = $_SERVER['HTTP_HOST'];
-            $userip = $_SERVER['REMOTE_ADDR'];
+            $userIP = $_SERVER['REMOTE_ADDR'];
             $status=1;
-            /* storing user login  */
-            $userL = mysqli_query($deal,"INSERT INTO userlog(userId,username,userip,status)
-                                          VALUES('$userid','$userName','$userip','$status') ");
-            header("location:dashboard.php")
-        }
-        else {
-            /* login unsuccessful */
-            $_SESSION['login'] = $_POST['usernam'];
-            $userip = $_SERVER['REMOTE_ADDR'];
+
+            /* for storing log if user login  successfull */
+            $addLog = mysqli_query($db_handle,"INSERT INTO  userlog(id,username, userip, status)
+                                               VALUES('$userID','$usernName','$userIP','$status')");
+            header("location:dashboard.php");
+        } else {
+            /* user login unsuccessfull */
+            $_SESSION['login']=$_POST['username'];
+            $userIP=$_SERVER['REMOTE_ADDR'];
             $status=0;
-            mysqli_query($deal,"INSERT INTO userlog(username,userip,status)
-                                 VALUES('$userName','$userip','$status')");
-            $_SESSION['errmsg'] = "Invalid username or password";
-            header("location:userLong.php")
+            mysqli_query($db_handle,"INSERT INTO userlog(username, userip, status)
+                                     VALUES('$userName','$userIP','$status') ");
+            $_SESSION("location:userLogin.php")
         }
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>User-Login</title>
-        <!-- custom style -->
-        <link rel="stylesheet" href="../assign/css/styles.css">
-        <link rel="stylesheet" href="../assign/css/plugins.css">
-        <link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
-    </head>
-<body>
+
+<body class="login">
     <div class="row">
-          <div class="main-login">
-                <div class="logo margin-top-30">
-                      <a href="../index.php"><h2> MMS | Patient Login</h2></a>
-                </div>
+          <div class="col-xs-10">
+                <div class="logo margin-top-30"><a href="../index.php"><h2> MMS | Patient Login</h2></a></div>
                 <div class="box-login">
                       <form class="form-login" method="post">
                             <fieldset>
-                                <legend>Sign in to your account</legend>
-                                    <p>Enter your name and password to log in.<br/>
-                                        <span style="color:red;">
-                                            <?php echo $_SESSION['errmsg'];?>
-                                            <?php echo $_SESSION['errmsg']="";?>
-                                        </span>
-                                    </p>
-                                    <div class="form-group">
-                                        <span class="input-icon">
-                                            <input name="username" type="text" class="form-control" placeholder="Username">
-                                            <i class="fa-fa-user"></i>
-                                        </span>
-                                    </div>
-                                    <div class="form-group form-actions">
-                                        <span class="input-icon">
-                                            <input name="password" type="password" class="form-control password" placeholder="Password">
+                                <legend>Sign to your account</legend>
+                                <p>Enter name and Password log in.<br/>
+                                    <span style="color:red;">
+                                        <?php echo $_SESSION['errmsg']; ?>
+                                        <?php echo $_SESSION['errmsg']; ?>
+                                    </span>
+                                </p>
+                                <div class="form-group">
+                                      <span class="input-icon">
+                                            <input type="password" name="username" class="form-control" placeholder="Username">
+                                            <i class="fa fa-user"></i>
+                                      </span>
+                                </div>
+                                <div class="form-group form-actions">
+                                      <span class="input-icon">
+                                            <input type="password" name="password" class="form-control password" placeholder="Password">
                                             <i class="fa fa-lock"></i>
-                                        </span>
-                                        <a href="forgotPassword.php">Forgot Password? </a>
-                                    </div>
-                                    <div class="form-actions">
-                                        <button name="submit" type="submit" class="btn btn-primary pull-right">Login
-                                                <i class="fa fa-arrow-circle-right"></i>
-                                        </button>
-                                    </div>
-                                    <div class="new-account">
-                                        Don't have an account yet? 
-                                        <a href="registration.php">Create an account</a>
-                                    </div>
-                             </fieldset>
+                                      </span>
+                                </div>
+                                <div class="form-actions">
+                                      <button type="submit" name="submit" class="btn btn-primary pull-right">
+                                           Login<i class="fa fa-arrow-circle-right"></i>
+                                      </button>
+                                </div>
+                                <div class="new-account">
+                                      Don't have an account yet?<a href="registration.php">Create an account</a>
+                                </div>                                
+                            </fieldset>
                       </form>
-                      <div class="">
+                      <div>
                             <span class="text-bold text-uppercase">Medical Management System</span>
                       </div>
                 </div>
           </div>
     </div>
-    <!-- custom javascript -->
-    <script src="../assign/js/main.js"></script>
-    <script src="../assign/js/login.js"></script>
+    <script src="assign/js/main.js"></script>
+    <script src="assign/js/login.js"></script>
     <script>
-            jquery(document).ready(function() {
-                Main.init();
-                Login.init();
-            })
+         jQuery(document).ready(function(){
+              Main.init();
+              Login.init();
+         });
     </script>
 </body>
-</html>
