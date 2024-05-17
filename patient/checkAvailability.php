b90283983718e78df078d5bb3bcc7b5871309958
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-05-17 11:28:29 -0700

    Add files via upload

--- patient/checkAvailability.php
+++ patient/checkAvailability.php
@@ -1,17 +1,13 @@
 <?php
     require_once('../define/config.php');
-    if(!empty($_POST['EMAIL'])){
-        $email=$_POST['email'];
-        $qUser = mysqli_query($deal,"SELECT email from users WHERE email='$email' ");
-        $cRows = mysqli_num_rows(qUser);
-        if($cRows > 0)
+    if(!empty($_POST['email'])){
+        $userEmail = $_POST['email'];
+        $qUser = mysqli_query($deal,"SELECT email FROM users WHERE email='$email' ");
+        $numRow = mysqli_num_rows(qUser);
+        if($numRows > 0)
         {
-            echo "<script style='color:red'>Email already exists.</script>";
-            echo "<script>$('#submit').prop('disabled', true);</script>";
-        }else 
-        {
-            echo "<span style='color:green'>Email available for Register.</span>";
-            echo "<script>$(#submit).prop('disabled',false);</script>";
+            echo "<script style='colr:green'>Email availale for registration.</script>";
+            echo "<script>$(#submit).prop('disabled', false);</script>";
         }
     }
 ?>
