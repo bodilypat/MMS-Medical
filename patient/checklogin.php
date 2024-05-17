6a0f305c72f6af9f7b9f72fa9db481924b9c5806
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-05-17 11:34:29 -0700

    Add files via upload

--- patient/checklogin.php
+++ patient/checklogin.php
@@ -1,12 +1,11 @@
 <?php
-    function check_login()
-    {
-        if(strlen($_SESSION['logi'])==0)
-        {
-            $host = $_SERVE['HTTP_HOST'];
+    function check_login(){
+        if(strlen($_SESSION['login']) == 0){
+            $host = $_SERVER['HTTP_HOST'];
             $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
             $extra = '../admin.php';
             $_SESSION['login']="";
-            header("Location: http://$host$uri/$extra");
+            header("location: http://$host$uri/$extra");
         }
     }
+?>
