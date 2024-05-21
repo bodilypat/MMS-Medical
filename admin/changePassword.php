fa139d03feffd4ca7dbd24331d737360bff79725
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-05-20 20:37:55 -0700

    Add files via upload

--- admin/changePassword.php
+++ admin/changePassword.php
@@ -1,148 +1,97 @@
 <?php
-	session_start();
-	//error_reporting(0);
-	include('../define/config.php');
-	if(strlen($_SESSION['id']==0)) {
-		header('location:logout.php');
-	} else{
-
-		date_default_timezone_set('America/Los_Angeles');// change according timezone
-		$currentTime = date( 'd-m-Y h:i:s A', time () );
-
-		if(isset($_POST['submit']))
-		{
-			$currentPass=$_POST['cpass'];	
-			$userName=$_SESSION['login'];
-			/* get password */
-			$qAd=mysqli_query($deal,"SELECT password FROM  admin WHERE password='$currentPass' && username='$userName'");
-			$result=mysqli_fetch_array($qAd);
-			if($result > 0)
-			{
-				$npass=$_POST['nPass'];
-			    $editAd=mysqli_query($deal,"UPDATE admin SET password='$npass', updationDate='$currentTime' WHERE username='$userName'");
-			    $_SESSION['msg1']="Password Changed Successfully !!";
-			}
-			else
-			{
-				$_SESSION['msg1']="Old Password not match !!";
-			}
-		}
+    session_start();
+    include('../define/config.php');
+    if(strlen($_SESSION['id']==0)){
+        header('location:logout.php');
+    } else {
+        date_default_timezone_set('America/Lost_Angeles'); /* change according timezone */
+        $currentTime = date('d-m-Y h:i:s A', time() );
+        if(isset($_POST['submit']))
+        { 
+            $currentPass = $_POST['cpass'];
+            $userName=$_SESSION['login'];
+            /* get password */
+            $qAdmin=mysqli_query($deal,"SELECT password FROM admi  WHERE password='$userPass' && username='$userName' ");
+            $result=msyqli_fetch_array($qAdmin);
+            if($result > 0)
+            {
+                $newPass=$_POST['newPassword'];
+                $eAdmin=mysqli_query($deal,"UPDATE admin SET password='$newPass', updatingDate='$currentTime' WHERE username='$userName' ");
+                $SESSION['msg']="Password changed Successfully !!";
+            } else {
+                $_SESSION['msg']="Old Password not match  !! ";
+            }
+        }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Admin | change Password</title>
-		<!-- CSS -->
-		<link rel="stylesheet" href="../assign/css/styles.css">
-		<link rel="stylesheet" href="../assign/css/plugins.css">
-		<link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
-		<script type="text/javascript">
-		function valid()
-		{
-			if(document.changpwd.cpass.value=="")
-			{
-				alert("Current Password Filed is Empty !!");
-				document.changpwd.cpass.focus();
-				return false;
-			}
-			else if(document.changpwd.npass.value=="")
-			{
-				alert("New Password Filed is Empty !!");
-				document.changpwd.npass.focus();
-				return false;
-			}
-			else if(document.changpwd.cfpass.value=="")
-			{
-				alert("Confirm Password Filed is Empty !!");
-				document.changpwd.cfpass.focus();
-				return false;
-			}
-			else if(document.changpwd.npass.value!= document.changpwd.cfpass.value)
-			{
-				alert("Password and Confirm Password Field do not match  !!");
-				document.changpwd.cfpass.focus();
-				return false;
-			}
-			return true;
-		}
-	</script>
-	</head>
+
+    <script type="text/javascript">
+         function valid()
+         {
+            if(document.changpwd.cpass.value=="")
+            {
+                alert("Current Password Filed is Empty !! ");
+                document.changpwd.cpass.focus();
+                return false;
+            }
+            else if(document.changpwd.npass.value=="")
+            {
+                alert("New Password filed is Empty !! ");
+                document.changpwd.npass.focus();
+                return false;
+            }
+            else if(document.changpwd.cfpass.value=="")
+            {
+                alert("Confirm Password Filed is Empty !! ");
+                document.changpwd.cfpass.focus();
+                return false;
+            } else if(document.changpwd.npass.value!=document.changpwd.cfpass.value)
+            {
+                alert("Password and Confirm Password Field do not match !! ");
+                document.changpwd.cfpass.focus();
+                return false;
+            }
+            return true;
+         }
+    </script>
+    <!-- box change password -->
 <body>
-	<div id="application">		
-		<?php include('../assign/sidebar.php');?>
-		<div class="application-content">				
-			<?php include('../assign/header.php');?>
-			</header>			
-			<div class="main-content" >
-				<div class="wrap-content container" id="container">
-					<!-- section: PAGE TITLE -->
-					<section id="page-title">
-						<div class="row">
-							<div class="col-sm-8"><h1 class="mainTitle">Admin | Change Password</h1></div>
-								<ol class="breadcrumb">
-									<li><span>Admin</span></li>
-									<li class="active"><span>Change Password</span></li>
-								</ol>
-						</div>
-					</section>					
-					<!-- section:CONTENT -->
-					<div class="container-fluid container-fullw bg-white">
-						<div class="row">
-							<div class="col-md-12">									
-								<div class="row margin-top-30">
-									<div class="col-lg-8 col-md-12">
-										<div class="panel panel-white">
-											<div class="panel-heading"><h5 class="panel-title">Change Password</h5></div>
-											<div class="panel-body">
-												<p style="color:red;">
-													<?php echo htmlentities($_SESSION['msg1']);?>
-													<?php echo htmlentities($_SESSION['msg1']="");?>
-												</p>	
-												<form name="changpwd" role="form"  method="post" onSubmit="return valid();">
-													<div class="form-group">
-														<label for="CurrentPassword">Current Password</label>
-														<input name="cpass" type="password"  class="form-control"  
-														       placeholder="Current Password">
-													</div>
-													<div class="form-group">
-														<label for="New Password">New Password</label>
-					                                    <input name="npass" type="password"  class="form-control"  
-														       placeholder="New Password">
-													</div>														
-													<div class="form-group">
-														<label for="ConfirmPassword">Confirm Password</label>
-														<input name="cfpass" type="password"  class="form-control"  
-														       placeholder="Confirm Password">
-													</div>	
-													<button name="submit" type="submit" class="btn btn-o btn-primary">
-														Submit
-													</button>
-												</form>
-											</div>
-										</div>
-									</div>											
-								</div>
-							</div>
-						<div class="col-lg-12 col-md-12">
-							<div class="panel panel-white"></div>
-						</div>
-	                </div>
-				</div>
-			</div>
-		</div>						
-	</div>	
-		<!--  FOOTER -->
-	<?php include('../define/footer.php');?>
-	<?php include('../define/setting.php');?>
-	<!-- JAVASCRIPT -->
-	<script src="../assign/js/main.js"></script>
-	<script src="../assign/js/form-elements.js"></script>
-	<script>
-		jQuery(document).ready(function() {
-			Main.init();
-			FormElements.init();
-			});
-	</script>
+    <div id="app">
+          <div class="panel panel-white">
+                <div class="panel-heading"><h5 class="panel-title">Change Password</h5></div>
+                <div class="panel-body">
+                      <p stype="color:red;">
+                          <?php echo htmlentities($_SESSION['msg']); ?>
+                          <?php echo htmlentities($_SESSION['msg']); ?>
+                      </p>
+                      <form name="changpwd" role="form" method="post" onSubmit="return valid(); ">
+                            <div class="form-group">
+                                 <label for="CurrentPassword">Current Password</label>
+                                 <input name="cpass" type="password" class="form-contrl" placeholder="Current Password">
+                            </div>
+                            <div class="form-group">
+                                  <label for="NewPassword">New Password</label>
+                                  <input name="npass" type="password" class="form-control" placeholder="New Password">
+                            </div>
+                            <div class="form-group">
+                                  <label for="ConfirmPassword">Confirm Password</label>
+                                  <input name="cfpass" type="password" class="form-control" placeholder="Confirm Password">
+                            </div>
+                            <button name="submit" type="submit" class="btn btn-o btn-primary">
+                                Submit
+                            </button>
+                      </form>
+                </div>
+          </div>
+    </div>
+    <script src="../assign/js/main.js"></script>
+    <script src="../assign/js/form-elements.js"></script>
+    <script>
+        jQuery(document).ready(function(){
+            Main.init();
+            FormElements.init();
+        });
+    </script>
 </body>
-</html>
-<?php } ?>
+<?php
+    }
+?>
