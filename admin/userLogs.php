05a7dce63a93a62b7f722739dace2e026f8e1759
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-03 14:56:46 -0700

    Add files via upload

--- admin/userLogs.php
+++ admin/userLogs.php
@@ -1,105 +1,97 @@
 <?php
-	session_start();
-	error_reporting(0);
-	include('include/config.php');
-	if(strlen($_SESSION['id']==0)) {
-		header('location:logout.php');
-	} else{
+    session_start();
+    error_reporting(0);
+    include('../define/config.php');
+    if(strlen($_SESSION['id'])) {
+        header('location: logout.php');
+    } else {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Admin | User Session Logs</title>
-		<!-- CSS -->
-		<link rel="stylesheet" href="assets/css/styles.css">
-		<link rel="stylesheet" href="assets/css/plugins.css">
-		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
-	</head>
+    <head>
+        <title>User Session Logs | Admin</title>
+        <!-- custom style -->
+        <link rel="stylesheet" href="../assign/css/styles.css">
+        <link rel="stylesheet" href="../assign/css/pluins.css">
+        <link rel="styelsheet" href="../assign/css/theme/theme-1.css" id="skin_color">
+    </head>
 <body>
-	<div id="app">		
-		<?php include('../define/sidebar.php');?>
-		<div class="app-content">				
-			<?php include('../define/header.php');?>
-			<div class="main-content" >
-				<div class="wrap-content container" id="container">
-					<!-- section : PAGE TITLE -->
-					<section id="page-title">
-						<div class="row">
-							<div class="col-sm-8"><h1 class="mainTitle">Admin  | User Session Logs</h1></div>
-							<ol class="breadcrumb">
-								<li><span>Admin </span></li>
-								<li class="active"><span>User Session Logs</span></li>
-							</ol>
-						</div>
-					</section>					
-					<!-- section: CONTENT -->
-					<div class="container-fluid container-fullw bg-white">						
-						<div class="row">
-							<div class="col-md-12">									
-								<p style="color:red;">
-								    <?php echo htmlentities($_SESSION['msg']);?>
-								    <?php echo htmlentities($_SESSION['msg']="");?>
-								</p>	
-								<table class="table table-hover" id="sample-table-1">
-									<thead>
-										<tr>
-											<th class="center">#</th>
-											<th class="hidden-xs">User id</th>
-											<th>Username</th>
-											<th>User IP</th>
-											<th>Login time</th>
-											<th>Logout Time </th>
-											<th> Status </th>																								
-										</tr>
-									</thead>
-									<tbody>
-										<?php
-										/* query userlog */
-											$qUl=mysqli_query($deal,"SELECT * FROM userlog ");
-											$count=1;
-											while($resultset=mysqli_fetch_array($qUl))
-											{
-										?>
-										<tr>
-											<td class="center"><?php echo $count;?>.</td>
-											<td class="hidden-xs"><?php echo $resultset['uid'];?></td>
-											<td class="hidden-xs"><?php echo $resultset['username'];?></td>
-											<td><?php echo $resultset['userip'];?></td>
-											<td><?php echo $resultset['loginTime'];?></td>
-											<td><?php echo $resultset['logout'];?></td>												
-											<td>
-												<?php if($resultset['status']==1)
-												{
-													echo "Success";
-												}
-												else
-												{
-													echo "Failed";
-												}?>
-
-											</td>												
-										</tr>											
-										<?php $count=$count+1;  }?>																						
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>				
-				</div>
-			</div>
-		</div>
-		<!-- FOOTER -->
-		<?php include('include/footer.php');?>		
-		<?php include('include/setting.php');?>
-	</div>
-	<script src="assets/js/main.js"></script>	
-	<script src="assets/js/form-elements.js"></script>
-	<script>
-			jQuery(document).ready(function() {
-			Main.init();
-			FormElements.init();
-		});
-	</script>	
+    <div id="app">
+        <?php include('../define/sidebar.php');?>
+        <div class="app-content">
+            <?php include('../define/header.php');?>
+            <div class="main-content">
+                <div class="wrap-content container" id="container">
+                    <section id="page-title">
+                        <div class="row">
+                            <div class="col-sm-8"><h1 class="mainTitle">User Session Logs | Admin <h1></div>
+                            <ol class="breadcrumb">
+                                 <li><span>Admin</span></li>
+                                 <li class="active"><span>User Session Logs</span></li>
+                            </ol>
+                        </div>
+                    </section>
+                    <!-- content -->
+                    <div class="container-fluid container-fullw bg-white">
+                        <div class="row">
+                            <p style="color:red;">
+                                <?php echo htmlentities($_SESSION['msg']);?>
+                                <?php echo htmlentities($_SESSION['msg']=="");?>
+                            </p>
+                            <table class="table table-hover" id="userLog-table">
+                                <thead>
+                                     <tr>
+                                           <th class="center">#</th>
+                                           <th class="hidden-xs">User ID</th>
+                                           <th>Username</th>
+                                           <th>User IP</th>
+                                           <th>Login time</th>
+                                           <th>Logout Time</th>
+                                           <th>Status</th>
+                                     </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                    $qUser = mysqli_query($deal,"SELECT * FROM userLog");
+                                    $count = 1;
+                                    while($result = mysqli_fetch_array($qUser)) {
+                                ?>
+                                     <tr>
+                                           <td class="center"><?php echo $count;?>.</td>
+                                           <td class="hidden-xs"><?php echo $result['userID'];?></td>
+                                           <td class="hidden-xs"><?php echo $result['userName'];?></td>
+                                           <td><?php echo $result['userIP'];?></td>
+                                           <td><?php echo $result['loginTime'];?></td>
+                                           <td><?php echo $result['logout'];?></td>
+                                           <td>
+                                                <?php if($result['']==1){
+                                                    echo 'success';
+                                                } else {
+                                                    echo 'Failed';
+                                                } ?>
+                                           </td>
+                                     </tr>
+                                    <?php $count $count+1; }?>
+                                </tbody>
+                            </table>                     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- FOOTER -->
+        <?php include('../define/footer.php');?>
+        <?php include('../define/setting.php');?>
+    </div>
+    <!-- custom javascript -->
+    <script src="../assign/js/main.js"></script>
+    <script src="../assign/js/formElements.js"></script>
+    <script>
+        jQuery(document).ready(function(){
+            Main.init();
+            FormElement.init();
+        });
+    </script>
 </body>
 </html>
-<?php } ?>
+    }
