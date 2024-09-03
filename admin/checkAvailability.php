75544d2dc9498228aa4ba776c472e57a7a0486d2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-03 14:51:47 -0700

    Add files via upload

--- admin/checkAvailability.php
+++ admin/checkAvailability.php
@@ -1,16 +1,15 @@
 <?php
-    requrie_once("define/config.php");
-    if(!empty($_POST['emailid'])){
-        $email=$_POST['emailid'];
-        $qDoc=mysqli_query($deal,"SELECT doctorEmail FROM doctors WHERE doctorEmail='$email' ");
-        $count=mysqli_num_rows($qDoc);
-        if($count > 0)
-        {
-            echo "<script style='color:red'>Email already exists .</script>";
-            echo "<script>$('#submit').prop('disabled', true);</script>";
+    require_once('../define/config.php');
+    if(!empty($_POST['email'])) {
+        $email = $_POST['email'];
+        $qDoc = mysqli_query($deal,"SELECT docEmail FROM doctors WHERE docEmail = '$email' ");
+        $count = mysqli_num_rows($qDoc);
+        if($count > 0) {
+            echo '<span style="color:red">Email already exists.</span>';
+            echo '<script>$('submit').prop('disabled',true);</script>';
         } else {
-            echo "<script style='color:green'>Email available for Registration .</span>";
-            echo "<script>$('#submit').prop('disabled', false);</script>";
+            echo '<span style='color:green'>Email available for Registration .</span>';
+            echo '<script>$("#submit").prop('disabled',false);</script>';
         }
     }
 ?>
