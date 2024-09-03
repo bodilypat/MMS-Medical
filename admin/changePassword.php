0f41eb8a04305cad3a84eee52a6fe4c66823919b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-03 14:51:28 -0700

    Add files via upload

--- admin/changePassword.php
+++ admin/changePassword.php
@@ -1,97 +1,123 @@
 <?php
-    session_start();
-    include('../define/config.php');
-    if(strlen($_SESSION['id']==0)){
+     session_strat();
+     inlude('../define/config.php');
+     if(strlen(_$_SESSION['id']==0)) {
         header('location:logout.php');
-    } else {
-        date_default_timezone_set('America/Lost_Angeles'); /* change according timezone */
-        $currentTime = date('d-m-Y h:i:s A', time() );
+     } else {
+        date_default_timezone_set("America/Monterrey"); /* change according timezone */
+        $current_time = date('d-m-Y h:i:s A', time() );
         if(isset($_POST['submit']))
-        { 
-            $currentPass = $_POST['cpass'];
+        {
+            $cPass = $_POST['cPassword'];
             $userName=$_SESSION['login'];
-            /* get password */
-            $qAdmin=mysqli_query($deal,"SELECT password FROM admi  WHERE password='$userPass' && username='$userName' ");
-            $result=msyqli_fetch_array($qAdmin);
+            $qAd=mysqli_query($deal,"SELECT password FROM admin WHERE password='$cPass' && username='$userName' ");;
+            $result=mysqli_fetch_array($qAd)
             if($result > 0)
             {
-                $newPass=$_POST['newPassword'];
-                $eAdmin=mysqli_query($deal,"UPDATE admin SET password='$newPass', updatingDate='$currentTime' WHERE username='$userName' ");
-                $SESSION['msg']="Password changed Successfully !!";
+                $nPass = $_POST['npassword'];
+                $editAd = mysqli_query($deal,"UPDATE admin SET password='$nPass', updationDate='$current_time' WHERE username='$userName'");
+                $_SETTION['msg'] = "Password change Successfullty";
             } else {
-                $_SESSION['msg']="Old Password not match  !! ";
+                $_SESSION['msg'] ="Old Password not match !!";
             }
         }
 ?>
-
-    <script type="text/javascript">
-         function valid()
-         {
-            if(document.changpwd.cpass.value=="")
-            {
-                alert("Current Password Filed is Empty !! ");
-                document.changpwd.cpass.focus();
-                return false;
-            }
-            else if(document.changpwd.npass.value=="")
-            {
-                alert("New Password filed is Empty !! ");
-                document.changpwd.npass.focus();
-                return false;
-            }
-            else if(document.changpwd.cfpass.value=="")
-            {
-                alert("Confirm Password Filed is Empty !! ");
-                document.changpwd.cfpass.focus();
-                return false;
-            } else if(document.changpwd.npass.value!=document.changpwd.cfpass.value)
-            {
-                alert("Password and Confirm Password Field do not match !! ");
-                document.changpwd.cfpass.focus();
-                return false;
-            }
-            return true;
-         }
-    </script>
-    <!-- box change password -->
-<body>
-    <div id="app">
-          <div class="panel panel-white">
-                <div class="panel-heading"><h5 class="panel-title">Change Password</h5></div>
-                <div class="panel-body">
-                      <p stype="color:red;">
-                          <?php echo htmlentities($_SESSION['msg']); ?>
-                          <?php echo htmlentities($_SESSION['msg']); ?>
-                      </p>
-                      <form name="changpwd" role="form" method="post" onSubmit="return valid(); ">
-                            <div class="form-group">
-                                 <label for="CurrentPassword">Current Password</label>
-                                 <input name="cpass" type="password" class="form-contrl" placeholder="Current Password">
-                            </div>
-                            <div class="form-group">
-                                  <label for="NewPassword">New Password</label>
-                                  <input name="npass" type="password" class="form-control" placeholder="New Password">
-                            </div>
-                            <div class="form-group">
-                                  <label for="ConfirmPassword">Confirm Password</label>
-                                  <input name="cfpass" type="password" class="form-control" placeholder="Confirm Password">
-                            </div>
-                            <button name="submit" type="submit" class="btn btn-o btn-primary">
-                                Submit
-                            </button>
-                      </form>
-                </div>
-          </div>
-    </div>
-    <script src="../assign/js/main.js"></script>
-    <script src="../assign/js/form-elements.js"></script>
-    <script>
-        jQuery(document).ready(function(){
-            Main.init();
-            FormElements.init();
-        });
-    </script>
-</body>
+<!-- validate: script -->
+<script type="text/javascript">
+     function valid()
+     {
+        if(document.changepwd.cpassword.value=="")
+        {
+            alert("Current Password filed is Empty !!");
+            document.changepwd.cpassword.focus();
+            return false;
+        }
+        else if(document.changepwd.npassword.value=="")
+        {
+            alert("New Password failed is Empty !!");
+            document.changepwd.npassword.focus();
+            return false;
+        } else if(document.changepwd.cfpassword.value=="")
+        {
+            alert("Confirm Password Filed is Empty !!");
+            document.changepwd.cfpassword.focus();
+            return false;
+        } else if(document.changepwd.npassword.value!=document.changepwd.cfpassword.value)
+        {
+            alert("Password and Confirm Password Filed do not match !!");
+            decoument.changepwd.cfpassword.focust();
+            return false;
+        }
+        return true;
+     }
+</script>
+<!-- body -->
+     <div id="app">
+           <div class="app-content">
+                 <div class="main-content">
+                       <div class="wrap-content container" id="container">
+                             <!-- page title -->
+                             <section id="page-title">
+                                  <div class="row">
+                                        <div class="col-sm-8"><h1 class="mainTitle">Admin | Change Password</h1></div>                                  
+                                        <ol class="breadcrumb">
+                                            <li><span>Admin</span></li>
+                                            <li class="active"><span>Change Password</span></li>
+                                        </ol>
+                                    </div>
+                             </section>
+                             <!-- content -->
+                             <div class="container-fluid container-fullw bg-white">
+                                   <div class="row">
+                                         <div class="col-md-12">
+                                              <div class="row margin-top-30">
+                                                    <div class="col-lg-8 col-md-12">
+                                                         <div class="panel panel-white">
+                                                              <div class="panel-heading">Change Password</div>
+                                                         </div>
+                                                         <div class="panel-body">
+                                                               <p stype="color:red;">
+                                                                    <?php echo htmlentities($_SESSION['msg']);?>
+                                                                    <?php echo htmlentities($_SESSION[msg]=="");?>
+                                                               </p>
+                                                               <form role="form" name="changepwd" method="post" onSubmit="return valid(); ">
+                                                                      <div class="form-group">
+                                                                            <label></label>
+                                                                            <input type="password" name="cpassword" class="form-control" placehodler="New Password">
+                                                                      </div>
+                                                                      <div class="form-group">
+                                                                            <label for="NewPassword">New Password</label>
+                                                                            <input type="password" name="npass" class="form-control" placeholder="New Password">
+                                                                      </div>
+                                                                      <button type="submit" name="submit" class="btn btn-o btn-primary">
+                                                                            Submitt
+                                                                      </button>
+                                                               </form>
+                                                         </div>
+                                                    </div>
+                                              </div>
+                                         </div>
+                                   </div>
+                                   <div class="col-lg-12 col-md-12">
+                                         <div class="panel panel-white"></div>
+                                   </div>
+                             </div>
+                       </div>
+                 </div>
+           </div>
+     </div>
+     <!-- footer -->
+     <?php include('../define/footer.php');?>
+     <?php include('../define/setting.php');?>
+     <!-- javascript -->
+     <script src="../assign/js/main.js"></script>
+     <script src="../assign/js/form-elements.js"></script>
+     <script>
+          jQuery(document).ready(function() {
+               Main.init();
+               FormElement.init();
+          });
+     </script>
 <?php
-    }
+     } 
 ?>
