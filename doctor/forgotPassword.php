e73b2b1e5c540122e002224947e4ad507ae6e12f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-04 16:21:29 -0700

    Add files via upload

--- doctor/forgotPassword.php
+++ doctor/forgotPassword.php
@@ -2,77 +2,74 @@
     session_start();
     error_reporting(0);
     include('../define/config.php');
-    /* checking reset password */
-    if(isset($_POST['submit']))
-    {
-        $userName = $_POST['fullname'];
-        $userEmail = $_POST['email'];
-        $qUser = mysqli_query($deal,"SELECT id FROM users WHERE fullname='$name' AND email='$userEmail' ");
-        $result=mysqli_num_rows($qUser);
-        if($result > 0)
-        {
-            $_SESSION['fullName'] = $userName;
-            $_SESSION['email'] = $userEmail;
-            header('location:resetPassword.php') ;
+    /* check details for reset password */
+    if(isset($_POST['submit'])) {
+        $contactNo = $_POST['contactno'];
+        $docEmail = $_POST['email'];
+        $qDoc = mysqli_query($deal,"SELECT * id FROM doctors WHERE contactno = '$contactno' and doctorEmail ='$docEmail' ");
+        $result = mysqli_num_rows($qDoc);
+        if($ressult > 0) {
+            $_SESSION['contactno'] = $contactNo;
+            $_SESSION['doctorEmail'] = $docEmail;
+            header('location: resetPassword.php');
         } else {
-            echo '<script>alart("Invalid details. Please try with valid details");</script>';
-            echo '<script>window.location.href="forgotPassword.php"></script>';
+            echo "<script>alert('Invalid details. Please try to valid details');</script>";
+            echo "<script>window.location.href = 'forgotPassword.php'</script>";
         }
     }
 ?>
+
 <!DOCTYPE html>
-<html leng="en">
+<html lang="en">
     <head>
-          <title>Patient Password Recovery</title>
-          <link rel="stylesheet" href="assign/css/styles.css">
-          <link rel="stylesheet" href="assign/css/plugin.css">
-          <link rel="stylesheet" href="assign/css/theme-1.css" id="skin-color">
+        <title>Password Recovery</title>
+        <!-- custom style -->
+        <link rel="stylesheet" href="../assign/css/styles.css">
+        <link rel="stylesheet" href="../assign/css/plugins.css">
+        <link rel="stylesheet" href="../assign/css/themes/theme-1.css" id="skin_color" />
     </head>
 <body class="login">
-      <div class="row">
-            <div class="main-login">
-                  <div class="logo margin-top-30"><a href="../index.php">MMS | Patient Password Recovery</h2></a></div>
-                  <div class="box-login">
-                        <form class="form-login" metho="post">
-                              <fieldset>
-                                    <legend>Patient Password Recovery</legend>
-                                    <p>Please enter your Email and password to recover your password<ber/></p>
-                                    <div class="form-group form-actions">
-                                          <span clsss="input-icon">
-                                                <input name="fullname" type="text" class="form-control" placeholder="Register Full Name">
-                                                <i class="fa fa-lock"></i>
-                                          </span>
-                                    </div>
-                                    <div class="form-group">
-                                         <span class="input-icon">
-                                               <input name="email" type="email" class="form-control" placeholder="Registration Email">
-                                               <i class="fa fa-user"></i></span>
-                                         </span>
-                                    </div>
-                                    <div class="form-actions">
-                                          <button name="submit" type="submit" class="btn btn-primary pull-right">Reset
-                                                <i class="fa fa-arrow-circle-right"><i>
-                                          </button>
-                                    </div>
-                                    <div class="form-account">Already have an account
-                                         <a href="userLogin.php">Log-in</a>
-                                    </div>
-                              </fieldset>
-                        </form>
-                        <div class="">
-                             <span class="text-bold text-uppercase">Medical Management System</span>
+    <div class="row">
+        <div class="main-login">
+            <div class="logo margin-top-30"><a href="../../index.php"><h2>MMS | Doctor Password Recovery</h2></a></div>
+            <div class="box-login">
+                <form class="form-login" method="post">
+                    <fieldset>
+                        <legend>Doctor Password Recovery</legend>
+                        <p>Enter contact number and email to recovery your password.<br /></p>
+                        <div class="form-group form-action">
+                            <span class="input-icon">
+                                <input type="text" class="form-control" name="contactno"  placeholder="Registred Contact Number">
+                                <i class="fa fa-lock"></i>
+                            </span>
                         </div>
-                  </div>
+                        <div class="form-actiion">
+                            <span class="input-icon">
+                                <input type="email" class="form-control"  name="email" placeholder="Registred Email">
+                                <i class="fa fa-user"></i>
+                            </span>
+                        </div>
+                        <div class="form-action">
+                            <button type="submit" class="btn btn-primary pull-right" name="submit">
+                                Reset<i class="fa fa-arrow-circle-right"></i>
+                            </button>
+                        </div>
+                    </fieldset>
+                </form>
+                <div class="">
+                    <span class="text-bold text-uppercase">Medical Management System</span>
+                </div>
             </div>
-      </div>
-      <!-- custom javascript -->
-      <script src="../assign/js/main.js"></script>
-      <script src="../assign/js/login.js"></script>
-      <script>
-            jQuery(document).ready(function(){
-                Main.init();
-                Login.init();
-            });
-      </script>
+        </div>
+    </div>
+    <!-- custom javascript -->
+    <script src="../assign/js/main.js"></script>
+    <script src="../assign/js/formElements.js"></script>
+    <script>
+        jQuery(document).ready(function(){
+            Main.init();
+            Login.init();
+        });
+    </script>
 </body>
 </html>
