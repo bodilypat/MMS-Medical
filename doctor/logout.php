fd80c05560e40be4a76f8a64770fcfdfb73a2301
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-04 14:08:54 -0700

    Add files via upload

--- doctor/logout.php
+++ doctor/logout.php
@@ -1,15 +1,15 @@
 <?php
     session_start();
     include('../define/config.php');
-    $_SESSION['dlogin']=="";
-    date_default_timezone_set('America/Los_Angeles');
-    $ldate=date( 'd-m-Y h:i:s A', time () );
-    $docid=$_SESSION['id'];
-    mysqli_query($deal,"UPDATE doctorslog  SET logout = '$ldate' WHERE uid = '$docid' ORDER BY id DESC LIMIT 1");
+    $_SESSION['dlogin'] = "";
+    date_default_timezone_set('America/Monterrey');
+    $lastDate = date('d-m-y h:i:s A', time() );
+    $docID = $_SESSION['id'];
+    mysqli_query($deal,"SELECT doctorlog SET logout = '$lastDate' WHERE userID = '$docID' ORDER BY id DESC LIMIT 1");
     session_unset();
-    //session_destroy();
-    $_SESSION['errmsg']="You have successfully logout";
+    /* session_destroy */
+    $_SESSION['errmsg'] = "You have successfully logout";
 ?>
-<script language="javascript">
-do  cument.location="../index.php";
+<script language = "javascript">
+    document.location = "../../index.php";
 </script>
