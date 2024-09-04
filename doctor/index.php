b8d7748363ca70100ad4c2e24c217c01239f41a0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-04 14:06:23 -0700

    Add files via upload

--- doctor/index.php
+++ doctor/index.php
@@ -1,78 +1,94 @@
 <?php
     session_start();
+    include('../define/config.php');
     error_reporting(0);
-    include('../define/config');
-    if(isset($_POST['submit'])){
-        $docName=$_POST['username'];
-        $docPass=$_POST['password'];
-        $qDoc=mysqli_query($deal,"SELECT * FROM doctors WHERE doctorEmail='$docName' AND password='docPass' ");
-        $result=mysqli_fetch_array($qDoc);
-        if($result > 0){
-            $_SESSION['dlogin']=$_POST['username'];
-            $_SESSION['id']=$result['id'];
-            $docID=$result['id'];
-            $docIP=$_SERVER['REMOTE_ADDR'];
-            $status=1;
-            $adlog=mysqli_query($deal,"INSERT INTO doctorslog(userId, username, userip, status)
-                                       VALUES('$docID','$docName','$docIP','$status') ");
+    if(isset($_POST['submit'])) {
+        $userName = $_POST['username'];
+        $docPass = $_POST['password'];
+        $qDoc = mysqli_query($deal,"SELECT * FROM doctors WHERE doctorEmail = '$userName' and password='$docPass' ");
+        $result = mysqli_fetch_array($qDoc);
 
-        } else{
-            $docIP=$_SERVER['REMOTE_ADDR'];
-            $status=0;
-            mysqli_query($deal,"SELECT INTO doctorlog(username,userip, status)
-                                VALUES('$docName','$docIP','$status') ");
-            header("location: index.php");
+        if($result > 0) {
+            $_SESSION['dlogin'] = $_POST['username'];
+            $_SESSION['id'] = $result['id'];
+            $userID = $result['id'];
+            $userIP = $_SERVER['REMOTE_ADDR'];
+            $status = 1;
+            $qDocl = mysqli_query($deal,"INSERT INTO doctorlog(userID, username, userIP, status)
+                                         VALUES('$userID','$userName','$userID','$userIP','$status' ");
+            header('location : dashboard.php');
+        } else {
+            $userIP = $_SERVAR['ROMOTE_ADDR'];
+            $status = 0;
+            mysqli_query($deal,"INSERT INTO doctorlog(username, userIP, status)VALUES('$username','$userIP','$status') ");
+            $_SESSION['errmsg'] = "Invalid username or password";
+            header('location: index.php');
         }
     }
 ?>
-<body class="login">
-    <div class="row">
-          <div class="main-login">
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Doctor Login</title>
+        <!-- custom style -->
+        <link rel="stylesheet" href="../assign/css/styles.css">
+        <link rel="stylesheet" href="../assign/css/plugins.css">
+        <link rel="stylesheet" href="../assign/css/thems/theme-1.css" id="skin_color" />
+    </head>
+<body>
+    <div class="login">
+        <div class="row">
+            <div class="main-login">
                 <div class="logo margin-top-30">
-                      <a href="../index.php"><h2>Medical | Doctor Login</h2></a>
+                    <a href="../../index.php"> MMS | Doctor Login</h2></a>
                 </div>
                 <div class="box-login">
-                      <form class="form-login" method="post">
-                            <fieldset>
-                                  <legend>Sign in to your account</legend>
-                                  <p>Please enter your name and password to log in.</br >
-                                       <span style="color:red;">
-                                            <?php echo $_SESSION['errmsg'];?>
-                                            <?php echo $_SESSION['errmsg']="";?>
-                                       </span>
-                                  </p>
-                                  <div class="form-group">
-                                        <span class="input-icon">
-                                             <input name="password" type="text" class="form-control" placeholder="Password">
-                                             <i class="fa fa user"></i>
-                                        </span>
-                                  </div>
-                                  <div class="form-group">
-                                        <span class="input-icon">
-                                             <input name="password" type="password" class="form-control password" placeholder="Password">
-                                             <in class="fa fa-lock"></i>
-                                        </div>
-                                  </div>
-                                  <div class="form-actions">
-                                        <button name="submit" type="submit" class="btn btn-primary pull-right">
-                                               Login<i class="fa fa-arrow-circle-right"></i>
-                                        </button>
-                                  </div>
-                            </fieldset>
-                      </form>
-                      <div>
-                            <span class="text-bold text-uppercase">Medical Management System</span>
-                      </div>
+                    <form class="form-login" method="post">
+                        <fieldset>
+                            <legend>Sign in to your account</legend>
+                            <p>Enter username and password . <br />
+                                <span style="color:red;">
+                                     <?php echo $_SESSION['errmsg'];?>
+                                     <?php echo $_SESSION['errmsg']=="";?>
+                                </span>
+                            </p>
+                            <div class="form-group"></div>
+                            <div class="form-group form-action">
+                                <span class="input-icon">
+                                    <input type="text" class="form-control" name="username" placeholder="Username">
+                                    <i class="fa fa-user"></i></span> 
+                                </span>
+                            </div>
+                            <div class="form-group form-action">
+                                <span class="input-icon">
+                                    <input type="password" class="form-control password" name="password" placeholder="Password">
+                                    <i class="fa fa-lock"></i>
+                                </span>
+                                <a href="forgotPassword.php">Forgot Password ?</a>
+                            </div>
+                            <div class="form-action">
+                                <button type="submit" class="btn btn-primary pull-right" name="submit" >
+                                    Login <i class="fa fa-arrow-circle-right"></i>
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                    <div class="">
+                        <span class="text-bold text-uppercase">Medical Management System</span>
+                    </div>
                 </div>
-          </div>
+            </div>
+        </div>
     </div>
-    <!-- javascript -->
-    <script src="../assets/js/min.js"></script>
-    <script src="../assets/js/login.js"></script>
+    <!-- custom javascript -->
+    <script src="../assign/js/main.js"></script>
+    <script src="../assign/js/login.js"></script>
     <script>
-         jQuery(document).ready(function(){
-             Main.init();
-             Login.init();
-         });
+        jQuery(document).ready(function(){
+            Main.init();
+            Login.init();
+        });
     </script>
 </body>
+</html>
