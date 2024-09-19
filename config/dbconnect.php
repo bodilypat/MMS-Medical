b1a6a072319d9cf1076cf06b6801cf67a17988a2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-19 12:55:48 -0700

    Create dbconnect.php

---
+++ config/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+    define('DB_SERVER','localhost');
+    define('DB_USER','root');
+    define('DB_password', '');
+    define('DB_NAME', 'medical');
+
+    $db_handle = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    /* check connect */
+    if(mysqli_connect_error()){
+        echo "Failed to connect to MySQL: " . mysql_connect_error();
+    }
+?>
+
