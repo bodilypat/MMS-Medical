e5763b54e16264c6d3a58839f9a95c898a856bce
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-23 15:39:57 -0700

    Add files via upload

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,1 +1,10 @@
-define footer
+<footer>
+    <div class="footer-inner">
+        <div class="pull-left">
+            <span class="text-bold text-uppercase">Medical Management System</span>
+        </div>
+        <div class="pull-right">
+            <span class="go-top"><i class="ti-angle-up"></i></span>
+        </div>
+    </div>
+</footer>
