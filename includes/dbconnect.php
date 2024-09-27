925ae7e4a99dd3577fc4c7cbad24832dd168e8c2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-27 00:12:47 -0700

    Add files via upload

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,1 +1,13 @@
-conenct database
+<?php
+    $host = 'localhost';
+    $db = 'medical_namagement';
+    $user = 'root';
+    $pass = '';
+try {
+        $pdo = new PDO("mysql:host=$host;dbname:$db", $user, $pass);
+        $pdo->setAttibute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+
+        die("Connection failed: " $e->getMessage()) ;
+    }
+?>
