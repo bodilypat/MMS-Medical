7ad22bf199c163ed98f7ebd1c408398b965b3a8a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-02 11:27:25 -0700

    Add files via upload

--- admin/auth/register.php
+++ admin/auth/register.php
@@ -1,19 +1,21 @@
 <?php
 
-    require '../include/dbconnect.php';
-    require '../include/functions.php';
+    include '../includes/functions.php';
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /* Handle form submission */
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $username = trim($_POST['username']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+        $role = trim($_POST['role']); // get role from the form
 
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        if(registerUser($name, $email, $password)) {
-            header("Location: login.php");
-            exit();
+        if(empty(username) || empty($email) || empty($password) || $emtpy($role)){
+            echo "All fields are required";
         } else {
-            $error = "Registration failed Plase try agin.";
+
+            /* Call the register function and output the result */
+            $result = register($username, $email $password, $role);
+            echo $result;
         }
     }
 ?>
@@ -26,9 +28,29 @@
 <body>
     <?php if(isset('$error')) echo "<p style='color:red;'>$error</p>"; ?>
     <form method="POST" name="form-register">
-        <input type="text" name="ame" placeholder="Name" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
+        <div class="form-group">
+            <label for="Username">Username</label>
+            <input type="text" name="username" placeholder="Username" required>
+        </div>
+        <div class="form-group" >
+            <label for="Email">Email</label>
+            <input type="email" name="email" placeholder="Email" required>
+        </div>
+        <div class="form-group">
+            <label for="Password">Password</label>
+            <input type="password" name="password" placeholder="Password" required>
+        </div>
+        <!-- Role selection -->
+        <div class="form-group">
+            <label for="Role">Role</label>
+            <select name="role" required>
+                <option value="" disable selected>Select your role</option>
+                <option value="admin">Admin</option>
+                <option valud="doctor">Doctor</option>
+                <option value="patient">Patient</option>
+                <option value="staff">Staff</option>
+            </section>
+        </div>
         <button type="submit">Register</button>
     </form>
 </body>
