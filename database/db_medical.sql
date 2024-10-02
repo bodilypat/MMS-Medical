6c1cf528a1cb095461ecdb31f170e20483bc6f45
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-02 11:35:59 -0700

    Add files via upload

--- database/db_medical.sql
+++ database/db_medical.sql
@@ -1,8 +1,12 @@
+CREATE DATABASE MEDICAL;
+
+USE MEDICAL ;
+
 CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('admin','doctor','staff') NOT NULL,
+    role ENUM('admin','doctor','patient') NOT NULL,
     updationDate VARCHAR(255) NOT NULL
     created_at TIMESTAMP CURRENT_TIMESTAMP
 ); 
