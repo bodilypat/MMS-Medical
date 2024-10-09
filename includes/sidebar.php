5411c03fe948948c405025fa8ab8c954ed432e0a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-09 13:03:53 -0700

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,72 @@
+<div id="sidebar" class="sidebar app-aside" >
+	<div class="sidebar-container perfect-scrollbar">
+		<nav>						
+			<!-- start: MAIN NAVIGATION MENU -->
+			<div class="navbar-title">
+				<span>Main Navigation</span>
+			</div>
+			<ul class="main-navigation-menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-home"></i></div>
+							<div class="item-inner"><span class="title"> Dashboard </span></div>
+						</div>
+					</a>
+				</li>				
+				<li>
+					<a href="manage_doctors.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Doctors </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_Appointments.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Appointments </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_medicals.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title"> Manage Medical Records </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_prescription.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title"> Manage Prescriptions </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_patients.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-pencil-alt"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Patients </span>
+							</div>
+						</div>
+					</a>
+				</li>
+			</ul>
+			
+			<!-- end: CORE FEATURES -->						
+		</nav>
+	</div>
+</div>
