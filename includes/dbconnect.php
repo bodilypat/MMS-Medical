4ceb99dbeee677990d878f5f125ac6448c50cabc
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-11 10:03:45 -0700

    Add files via upload

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,13 +1,30 @@
 <?php
+function dbconnect(){
+    /* dbconnect.php */
     $host = 'localhost';
-    $db = 'medical_namagement';
+    $db = 'db_inventory';
     $user = 'root';
     $pass = '';
-try {
-        $pdo = new PDO("mysql:host=$host;dbname:$db", $user, $pass);
-        $pdo->setAttibute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e){
+    $charset = 'utf8mb4';
 
-        die("Connection failed: " $e->getMessage()) ;
+    /* Data source name */
+    $dsn = "mysql:host=$host;dbname=$db;charest=$charset";
+
+    /* Options for the PDO connection */
+
+    $options = [
+        PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAUT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARE   => false,
+    ];
+
+    try {
+        // Create a PDO instance
+        $pdo = new PDO($dsn, $user, $pass, $option);
+        echo "Connected successfully!";
+
+    } catch (\PDOException $e) {
+        // Handle connection errors
+        die("Connection failed: ", .$e->getMessage(), (int)$e->getCode());
     }
-?>
+}
