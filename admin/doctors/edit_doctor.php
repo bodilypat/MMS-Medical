35b19f0012efa8766b3fca04b283224629b4b09c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-11 10:21:52 -0700

    Add files via upload

--- admin/doctors/edit_doctor.php
+++ admin/doctors/edit_doctor.php
@@ -1,24 +1,25 @@
 <?php
-    require '../incldues/functions.php';
 
-    if(!isset($_GET['id'])){
+    include('../includes/functions.php');
+
+    if(!isset($_GET['id'])) {
         header("Location:view_doctors.php");
         exit();
     }
 
-    $doctors = getDoctor($_GET['id'])
+    $doctors = getDoctor($_GET['id']);
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $name = $_POST['name'];
         $email = $_POST['email'];
-        $specialization = $_POST['specialization'];
+        $specialty = $_POST['specialty'];
         $phone = $_POST['phone'];
 
-        if(updateDoctor($doctor['id'], $name, $email, $specialization, $phone)){
-            header('Location:view_doctors.php');
-            exist();
+        if(updateDoctor($doctor['id'], $name, $email, $specialty, $phone)) {
+            header("location:manage_doctors.php");
+            exit();
         } else {
-            $error = "Failed to update doctor.";
+            $error = "Failed to update doctor. ";
         }
     }
 ?>
@@ -27,18 +28,29 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Edit Doctpr</title>
+        <title>Edit Doctor</title>
     </head>
-<body>
-    <h2>Edit Doctor</h2>
-    <?php if(isset($error)) echo "<p style='color:read;'>$error</p>"; ?>
-    <form method="post" name="form-doctor">
-        <input type="text" name="name" value="<?php echo htmlspecialchars($doctor['name']);?>" requied>
-        <input type="email" name="email" value="<?php echo htmlspecialchars($doctor['email']);?>" required>
-        <input type="text" name="specialization" value="<?php htmlspecialchars($doctor['specialization']);?>" required>
-        <input type="text" name="phone" value="<?php htmlspecialchars($dotor['phone']);?>" required>
-        <button type="submit">Update Doctor</button>
-    </form>
-    <a href="view_doctors.php">Cancel</a>
-</body>
+    <body>
+        <h2>Edit Doctor</h2>
+        <?php if(isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+        <form method="post" name="form-doctor">
+            <div class="form-group">
+                <label for="Name">Name</lebel>
+                <input type="text" name="name" value="<?php  echo htmlspecialchars($doctor['name']);?>" required >
+            </div>
+            <div class="form-group">
+                <label for="Email">Email</label>
+                <input type="email" name="email" value="<?pho echo htmlspecialchars($doctor['email']);?>" required >
+            </div>
+            <div class="form-group">
+                <label for="Specialty">Specialization</label>
+                <input type="text" name="specialty" value="<?php echo htmlspecialchars($doctor['specialty']);?>">
+            </div>
+            <div class="form-group">
+                <label for="Phone">Phone</label>
+                <input type="text" name="phone" value="<?php echo htmlspecialchars($doctor['phone']);?>">
+            </div>
+        </form>
+        <a href="manage_doctors.php">Cancel</a>
+    </body>
 </html>
