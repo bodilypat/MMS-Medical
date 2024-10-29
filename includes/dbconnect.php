1a60e3a89df8b6d3133a3e3d52526261bb159b23
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-29 08:41:17 -0700

    Add files via upload

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -7,24 +7,16 @@ function dbconnect(){
     $pass = '';
     $charset = 'utf8mb4';
 
-    /* Data source name */
-    $dsn = "mysql:host=$host;dbname=$db;charest=$charset";
-
-    /* Options for the PDO connection */
-
-    $options = [
-        PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAUT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARE   => false,
-    ];
-
     try {
         // Create a PDO instance
-        $pdo = new PDO($dsn, $user, $pass, $option);
-        echo "Connected successfully!";
+        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        // set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Connected successfully"; // for testing purposes
 
     } catch (\PDOException $e) {
         // Handle connection errors
-        die("Connection failed: ", .$e->getMessage(), (int)$e->getCode());
+        echo "Connection failed: " .$e->getMessage();
     }
 }
