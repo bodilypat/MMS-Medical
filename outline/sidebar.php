a6f3c4fc21faf4058a6e980d7d1f8bde91b9836c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-01-17 11:50:03 -0800

    Add files via upload

--- outline/sidebar.php
+++ outline/sidebar.php
@@ -1,82 +1,68 @@
-<div id="sidebar" class="sidebar app-aside" >
-	<div class="sidebar-container perfect-scrollbar">
-		<nav>						
-			<!-- start: MAIN NAVIGATION MENU -->
-			<div class="navbar-title">
-				<span>Main Navigation</span>
-			</div>
-			<ul class="main-navigation-menu">
-				<li>
-					<a href="dashboard.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-home"></i></div>
-							<div class="item-inner"><span class="title"> Dashboard </span></div>
-						</div>
-					</a>
-				</li>				
-				<li>
-					<a href="manage_doctors.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-list"></i></div>
-							<div class="item-inner">
-								<span class="title">Manage Doctors </span>
-							</div>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="manage_appointments.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-list"></i></div>
-							<div class="item-inner">
-								<span class="title">Manage Appointments </span>
-							</div>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="manage_medical_records.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-list"></i></div>
-							<div class="item-inner">
-								<span class="title"> Manage Medical Records </span>
-							</div>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="manage_prescriptions.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-list"></i></div>
-							<div class="item-inner">
-								<span class="title"> Manage Prescriptions </span>
-							</div>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="manage_patients.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-pencil-alt"></i></div>
-							<div class="item-inner">
-								<span class="title">Manage Patients </span>
-							</div>
-						</div>
-					</a>
-				</li>
-                <li>
-					<a href="manage_billings.php">
-						<div class="item-content">
-							<div class="item-media"><i class="ti-pencil-alt"></i></div>
-							<div class="item-inner">
-								<span class="title">Manage billings </span>
-							</div>
-						</div>
-					</a>
-				</li>
-			</ul>
-			
-			<!-- end: CORE FEATURES -->						
-		</nav>
-	</div>
+<div class="sidebar app-aside" id="sidebar">
+    <div class="sidebar-container perfect-scrollbar">
+        <!-- Navigation menu -->
+        <div class="navbar-title"><span>Main Navigation</span></div>
+        <ul class="main-navigation-menu">
+            <li>
+                <a href="dashboard.php">
+                    <div class="item-content">
+                        <div class="item-media">
+                            <i class="ti-home"></i>
+                        </div>
+                        <div class="item-inner">
+                            <span class="title">Dashboard</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="appointmentHistory.php">
+                    <div class="item-content">
+                        <div class="item-media">
+                            <i class="ti-list"></i>
+                        </div>
+                        <div class="item-inner">
+                            <span class="title">Appointment History</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="javascript:void(0)">
+                    <div class="item-content">
+                        <div class="item-media">
+                            <i class="ti-user"></i>
+                        </div>
+                        <div class="item-inner">
+                            <span class="title">Patients</span>
+                            <i class="icon-arrow"></i>
+                        </div>
+                    </div>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="addPatient.php">
+                            <span class="title">Add Patient</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="managePatient.php">
+                            <span class="title">Manage Patient</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="search.php">
+                    <div class="item-content">
+                        <div class="item-media">
+                            <i class="ti-search"></i>
+                        </div>
+                        <div class="item-inner">
+                            <span class="title">Search</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
 </div>
