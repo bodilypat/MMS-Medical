c3bbfafe62b3a9e525e338fa934ca77679c0f8fe
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-04-01 12:29:42 -0700

    Update dbconnect.php

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,22 +1,14 @@
 <?php
-function dbconnect(){
-    /* dbconnect.php */
-    $host = 'localhost';
-    $db = 'dbmedical';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+$servername = "localhost";
+$username = "root"; // Change to your database username
+$password = ""; // Change to your database password
+$dbname = "dbmedical"; // Change to your database name
 
-    try {
-        // Create a PDO instance
-        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-        // set the PDO error mode to exception
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Create connection
+$deal = new mysqli($servername, $username, $password, $dbname);
 
-        echo "Connected successfully"; // for testing purposes
-
-    } catch (\PDOException $e) {
-        // Handle connection errors
-        echo "Connection failed: " .$e->getMessage();
-    }
+// Check connection
+if ($deal->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+?>
