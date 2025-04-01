13367b17fea706c7a74861041df9203bdaec73a0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-04-01 12:48:18 -0700

    Add files via upload

--- admin/doctors/read_doctors.php
+++ admin/doctors/read_doctors.php
@@ -1,37 +1,42 @@
 <?php
-    require '../includes/function.php';
-    $doctors = getDoctors();
+include 'config.php';
+
+$sql = "SELECT * FROM doctors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr>
+			<th>Doctor ID</th>
+			<th>Name</th>
+			<th>Specialization</th>
+			<th>Email</th>
+			<th>Phone Number</th>
+			<th>Department</th>
+			<th>Status</th>
+			<th>Actions</th>
+		</tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["doctor_id"] . "</td>";
+        echo "<td>" . $row["first_name"] . " " . $row["last_name"] . "</td>";
+        echo "<td>" . $row["specialization"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+        echo "<td>" . $row["phone_number"] . "</td>";
+        echo "<td>" . $row["department"] . "</td>";
+        echo "<td>" . $row["status"] . "</td>";
+        echo "<td>
+            <a href='update_doctor.php?id=" . $row["doctor_id"] . "'>Edit</a> |
+            <a href='delete_doctor.php?id=" . $row["doctor_id"] . "'>Delete</a>
+        </td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No doctors found.";
+}
+
+$conn->close();
 ?>
-<!DOCTYPE Hhtml>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Doctors List</title>
-    </head>
-<body>
-    <h2>Doctors List</h2>
-    <table border="1">
-        <tr>
-             <th>ID</th>
-             <th>Name</th>
-             <th>Email</th>
-             <th>Specialization</th>
-             <th>Phone</th>
-             <th>Action</th>
-        </tr>
-        <?php foreach($doctors as $doctors): ?>
-        <tr>
-            <td><?php echo htmlspecialchars($doctor['id']);?></td>
-            <td><?php echo htmlspecialchars($doctor['name']);?></td>
-            <td><?php echo htmlspecialchars($doctor['specialization']);?></td>
-            <td><?php echo htmlspecialchars($doctor['phone']);?></td>
-            <td>
-                <a href="edit_doctor.php?id=<?php echo $doctor['id'];?>">Edit</a>
-                <a href="delete_doctor.php?id=<?php echo $doctor['id'];?>" onClick="return confirm('Are you sure you want to delete this doctor?');">Delete</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-    <a href="add_doctor.php">Add New Doctor</a>
-</body>
-</html>
