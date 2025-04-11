8142763afa9c81d4907ff60a728ac601c4ff27b9
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-04-11 11:55:03 -0700

    Add files via upload

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,10 @@
 <footer>
-				<div class="footer-inner">
-					<div class="pull-left">
-			<span class="text-bold text-uppercase"> Patient Management System</span>
-					</div>
-					<div class="pull-right">
-						<span class="go-top"><i class="ti-angle-up"></i></span>
-					</div>
-				</div>
-			</footer>
+    <div class="footer-inner">
+        <div class="pull-left">
+            <span class="text-bold text-uppercase">Medical Management System</span>
+        </div>
+        <div class="pull-right">
+            <span class="go-top"><i class="ti-angle-up"></i></span>
+        </div>
+    </div>
+</footer>
