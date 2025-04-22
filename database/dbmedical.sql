f5e197638e5a14dbfd8ed8939a7213196f6b09ab
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-04-22 09:01:46 -0700

    Update dbmedical.sql

--- database/dbmedical.sql
+++ database/dbmedical.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'user') DEFAULT 'user' NOT NULL,
+    role ENUM('admin','doctor','patient', 'user') DEFAULT 'user' NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
