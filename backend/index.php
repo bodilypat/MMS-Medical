816b3be946e1bb980866648d48ae5ffa883dca9c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-04-24 13:51:34 -0700

    Add files via upload

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,13 @@
 <?php
 	/* index.php */
-	$path = $_SERVAR['REQUEST_URI'];
+	$request = $_SERVAR['REQUEST_URI'];
+	$method = $_SERVAR['REQUEST_METHOD'];
+	
+	/* Remove query string and normalize */
+	$path = parse_url($request, PHP_URL_PATH);
+	$path = str_replace('/backend/api/', '', $path);
+	
+	/* Route dispather */
 	switch (true) {
 		case str_contains($path,'/api/common/login'):
 			require 'api/common/users.php'; 
