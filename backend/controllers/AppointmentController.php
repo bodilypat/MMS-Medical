f11ef72042a22ad1b6613df18aaaa87b8519d278
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-05-08 05:49:19 -0700

    Add files via upload

--- backend/controllers/AppointmentController.php
+++ backend/controllers/AppointmentController.php
@@ -10,29 +10,50 @@
 		}
 		
 		public function handleRequest($method, $data, $queryParams) {
-			switch ($method) {
-				case 'GET': 
-					if (isset($queryParams['appointment_id'])) {
-						$result = $this->model->getById($queryParams['appointment_id']);
-					} else {
-						$result = $this->model->getAll();
+			try {
+				switch ($method) {
+					case 'GET':
+						if (isset($queryParams['appointment_id'])) {
+							$result = $this->model->getById($queryParams['appointment_id']);
+							if (!$result) {
+								sendResponse(404, ['message' => 'Appointment not found']);
+								return;
+							}
+						} else {
+							$result = $this->model->getAll();
+						}
+						sendResponse(200, $result);
+						break;
+					case 'POST':
+						if (!isset($data['date'], $data['time'], $data['client_id'])) {
+							sendResponse(400, ['messaage' => 'Missing required fields']);
+							return;
+						}
+						$this->model->create($data);
+						sendResponse(201, ['message' => 'Appointment created']);
+						break;
+					case 'PUT':
+						if (!isset($data['appointment_id']]) {
+							sendResponse(400, ['message' => 'Missing appointment ID']);
+							return;
+						}
+						$this->model->update($data);
+						sendResponse(200, ['message' => 'Appointment updated']);
+						break;
+					case 'DELETE':
+						if (!isset($data['appointment_id'])) {
+							rendResponse(400, ['message' => 'Missing appointment ID']);
+							return;
+						}
+						$this->model->delete($data['appointment_id']);
+						sendResponse(204);
+						break;
+					default:
+						sendResponse(405, ['message' => 'Method not allowed']);
 					}
-					sendResponse(200, $result);
-				case 'POST': 
-					$this->model->create($data);
-					sendResponse(201, ['message' => 'Appointment created']);
-					break;
-				case 'PUT':
-					$this->model->update($data);
-					sendResponse(200, ['message' => 'Appointment updated']);
-					break;
-				case 'DELETE':
-					$this->model->delete($data['appointment_id']);
-					sendResponse(200, ['message' => 'Appointment deleted']);
-					break;
-				default:
-					sendResponse(405, ['message' => 'Method not allowed']);
+				} catch (Exception $e) {
+					sendResponse(500, ['message' => 'Server error', 'error' => $e->getMessage()]);
+				}
 			}
 		}
-	}
 		
