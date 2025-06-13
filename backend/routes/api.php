ca8743c6b199c1212d727d679ee3702f6a0d3067
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-06-13 11:29:16 -0700

    Add files via upload

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -30,7 +30,7 @@
 	$labTestController = new LabTestController($pdo);
 	$insuranceController = new InsuranceController($pdo);
 	$paymentController = new PaymentController($pdo);
-	$prescriptionController = new precriptionController($pdo);
+	$prescriptionController = new PrecriptionController($pdo);
 	$pharmarcyController = new PharmarcyController($pdo);
 	
 	/* User Routes */
@@ -44,53 +44,53 @@
 	$router->get('/api/patients', [$patientController,'index']);
 	$router->get('/api/patients/{id}', [$patientController,'show']);
 	$router->post('api/patients', [$patientController,'store']);
-	$route->put('api/users/{id}', [$patientController,'update']);
+	$router->put('api/users/{id}', [$patientController,'update']);
 	$router->delete('api/users/{id}', [$userController,'destroy']);
 	
 	/* Doctor Routes */
 	$router->get('api/doctors', [$doctorController,'index']);
 	$router->get('/api/doctors/{id}', [$doctorController, 'show']);
-	$router->post('/api/doctors', [$doctorController,'store');
+	$router->post('/api/doctors', [$doctorController,'store']);
 	$router->put('/api/doctors/{id}', [$doctorController,'update']);
 	$router->delete('/api/appointments/{id}', [$doctorController,'destroy']);
 	
 	/* Appointment Routes */
-	$router->get('a/api/appointments', [$appointmentController,'index']);
-	$router->get('api/appointments/{id}', [$appointmentController,'show']);
+	$router->get('/api/appointments', [$appointmentController,'index']);
+	$router->get('/api/appointments/{id}', [$appointmentController,'show']);
 	$router->post('/api/appointments', [$appointmentController,'store']);
 	$router->put('/api/appointment/{id}', [$appointmentController,'update']);
 	$router->delete('api/appointments/{id}', [$appointmentController,'destroy']);
 	
 	/* Medical Records Routes */
 	$router->get('/api/medical_records', [$medicalRecordController,' index']);
-	$router->get('/api/medical_record/{id}', [$medicalRecordController,'store']);
+	$router->get('/api/medical_record/{id}', [$medicalRecordController,'show']);
 	$router->post('/api/medical_records', [$medicalRecordController,'store']);
 	$router->put('/api/medical_records/{id}', [$medicalRecordController,'update']);
 	$router->delete('/api/medical_records/{id}', [$medicalRecordController,'destroy']);
 	
 	/* Lab Tests Routes */
 	$router->get('/api/lab_tests', [$labTestController, 'index']);
-	$router->get('/api/lab_tests/{id}', [$labTestController,'store']);
+	$router->get('/api/lab_tests/{id}', [$labTestController,'show']);
 	$router->post('/api/lab_tests', [$labTestController,'store']);
 	$router->put('/api/lab_tests/{id}', [$labTestController, 'update']);
 	$router->delete('/api/lab_tests/{id}', [$labTestController, 'destroy']);
 	
 	/* Insurance Routes */
-	$router->get('api/insurance', [$insuranceController, 'index']);
+	$router->get('/api/insurance', [$insuranceController, 'index']);
 	$router->get('/api/insurance/{id}', [$insuranceController, 'show']);
 	$router->post('/api/insurance', [$insuranceController, 'store']);
-	$rotuer->put('/api/insurance/{id}', [$insuranceController, 'update']);
+	$router->put('/api/insurance/{id}', [$insuranceController, 'update']);
 	$router->delete('/api/insurance/{id}', [$insuranceController, 'destroy']);
 	
 	/* Payment Routes */
 	$router->get('/api/payments', [$paymentController, 'index']);
-	$router->get('/api/payemnts/{id}', [$paymentController, 'show']);
+	$router->get('/api/payments/{id}', [$paymentController, 'show']);
 	$router->post('/api/payments', [$paymentController, 'store']);
 	$router->put('/api/payments/{id}', [$paymentController, 'update']);
 	$router->delete('/api/payments/{id}', [$paymentController, 'destroy']);
 	
 	/* Prescriptions Routes */
-	$router->get('/api/prescriptions', [$prescriptController, 'index']);
+	$router->get('/api/prescriptions', [$prescriptionController, 'index']);
 	$router->get('/api/prescriptions/{id}', [$prescriptionController, 'show']);
 	$router->post('/api/prescriptions', [$prescriptionController, 'store']);
 	$router->put('/api/prescriptions/{id}', [$prescriptionController, 'update']);
@@ -104,7 +104,7 @@
 	$router->delete('/api/pharmacies/{id}', [$pharmacyController, 'destroy']);
 	
 	/* Auth Routes */
-	$router->post('api/login', functiion () use ($pdo) {
+	$router->post('/api/login', function () use ($pdo) {
 		$login = new \App\Auth\Login($pdo);
 		$login->handle();
 	});
