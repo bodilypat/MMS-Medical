b07216cdbb9d3a911210c7e6d5fce757cd728afa
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-06-13 11:57:06 -0700

    Add files via upload

---
+++ backend/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+	
+	use Core\Router;
+	use App\Controller\{
+		HomeController,
+		DashboardController,
+		AuthController
+	};
+	
+	require_once '../config/database.php';
+	
+	$router = new Router();
+	
+	/* Public Web Routes */
+	$router->get('/', [HomeController::class, 'index']); //Homepage
+	$router->get('/about', [HomeController::class, 'about']);
+	$router->get('/contact', [HomeController::class, 'contact']);
+	$router->post('/contact',  [HomeController::class, 'sendMessage']);
+	
+	/* Authentication Views */
+	$router->get('/login', [AuthController::class, 'showLoginForm']);
+	$router->post('/login', [AuthController::class, 'login']);
+	$router->get('/register', [AuthController::class, 'showRegisterForm']);
+	$router->post('/register', [AuhController::class, 'register']);
+	$router->get('/logout', [AuthController::class, 'logout']);
+	
+	/* Dashboard Views */
+	$router->get('/dashboard', [DashboardController::class, 'indx']);
+	
+	/* Protected Pages  */
+	$router->get('/admin/patients', [DashboardController::class, 'patients']);
+	$router->get('/admin/doctors', [DashboardController::class, 'doctors']);
+	$router->get('/admin/appointment', [DashboardController::class, 'appointments']);
+	
+	return $router;
+	
