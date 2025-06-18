548edaac7723326c97b7f273ce70e6ecf5d154a2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-06-18 10:40:13 -0700

    Add files via upload

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,26 +1,22 @@
 <?php
 
 	class Database {
-		private static $host = 'localhost';
-		private static $db_name = 'dbmedical';
-		private static $username = 'root';
 		private static $conn = null;
 		
 		public static function getConnection() {
 			if (self::$conn === null) {
+				$env = require __DIR__ . '/env.php';
+				
 				try {
 					self::$conn = new PDO (
-						"mysql:host=" . self::$host . ";dbname=" . self::$db_name . ";charset=utf8mb4",
-						self::$username,
-						self::$password
+						"mysql:host={$env['DB_HOST']}; dbname={$env['DB_NAME']};charset=utf8mb4",
+						$env['DB_USER'],
+						$env['DB_PASS']
 					);
 					self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				} catch (PDOException $e) {
 					http_response_code(500);
-					echo json_encode([
-						"error" => "Database connection failed.",
-						"details" => $e->getMessage();
-					]);
+					echo json_encode(['error' => 'DB error','details' => $e->getMessage()]);
 					exit();
 				}
 			}
